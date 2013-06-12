b4cdd712298050a82fd3af97658afb0febd18c16
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2013-06-11 20:03:42 -0400

    Removed 'drupalgap_' prefix from function names, changed hook_menu item paths due to limitation with router determination function.

--- modules/mvc/mvc.js
+++ modules/mvc/mvc.js
@@ -30,7 +30,7 @@ function collection_list_page(module, type) {
  */
 function collection_load(module, type) {
   try {
-    return JSON.parse(window.localStorage.getItem(drupalgap_mvc_get_collection_key('collection', module, type)));
+    return JSON.parse(window.localStorage.getItem(mvc_get_collection_key('collection', module, type)));
   }
   catch (error) {
     alert('collection_load - ' + error);
@@ -42,7 +42,7 @@ function collection_load(module, type) {
  */
 function collection_save(module, type, collection) {
   try {
-    window.localStorage.setItem(drupalgap_mvc_get_collection_key('collection', module, type), JSON.stringify(collection));
+    window.localStorage.setItem(mvc_get_collection_key('collection', module, type), JSON.stringify(collection));
   }
   catch (error) {
     alert('collection_save - ' + error);
@@ -53,34 +53,12 @@ function collection_save(module, type, collection) {
  * Given a bucket (e.g. collection, settings), module name and mvc model type,
  * this will return the local storage key used for the model type's item collection.
  */
-function drupalgap_mvc_get_collection_key(bucket, module, model_type) {
+function mvc_get_collection_key(bucket, module, model_type) {
   try {
     return 'mvc_' + bucket + '_' + module + '_' + model_type;
   }
   catch (error) {
-    alert('drupalgap_mvc_get_collection_key - ' + error);
-  }
-}
-
-/**
- * Implements hook_menu().
- */
-function mvc_menu() {
-  try {
-    var items = {
-      'collection/%/%/list':{
-        'page_callback':"collection_list_page",
-        "page_arguments":[1, 2]
-      },
-      'item/%/%/%':{
-        'page_callback':"item_view_page",
-        "page_arguments":[1, 2, 3]
-      }
-    };
-    return items;
-  }
-  catch (error) {
-    alert('mvc_menu - ' + error);
+    alert('mvc_get_collection_key - ' + error);
   }
 }
 
@@ -109,7 +87,7 @@ function mvc_install() {
         // For each model type...
         $.each(models, function(model_type, model){
             // Set the primary key 'id', the module name, and model type
-            // on the model fields.
+            // on the model fields. These are the mvc_model_system_fields().
             model.fields.id = {
               "type":"hidden",
               "title":"ID",
@@ -131,14 +109,14 @@ function mvc_install() {
             eval('drupalgap.mvc.models.' + module + '.' + model_type + ' = model;');
             // Save an empty collection to local storage for this model type.
             window.localStorage.setItem(
-              drupalgap_mvc_get_collection_key('collection', module, model_type),
+              mvc_get_collection_key('collection', module, model_type),
               '[]'
             );
             // Save settings for the collection to local storage. The auto
             // increment value represents an item id, we start counting at zero
             // since the collection is an array.
             window.localStorage.setItem(
-              drupalgap_mvc_get_collection_key('settings', module, model_type),
+              mvc_get_collection_key('settings', module, model_type),
               '{"auto_increment":0}'
             );
         });
@@ -157,6 +135,32 @@ function mvc_install() {
   }
 }
 
+/**
+ * Implements hook_menu().
+ */
+function mvc_menu() {
+  try {
+    var items = {
+      'collection/list/%/%':{
+        'page_callback':"collection_list_page",
+        "page_arguments":[2, 3]
+      },
+      'item/%/%/%':{
+        'page_callback':"item_view_page",
+        "page_arguments":[1, 2, 3]
+      }
+    };
+    return items;
+  }
+  catch (error) {
+    alert('mvc_menu - ' + error);
+  }
+}
+
+function mvc_model_system_fields() {
+  return ['id', 'module', 'type'];
+}
+
 // We'll need developer friendly front end functions, e.g.
 // model_load();
 // model_save();
@@ -170,10 +174,10 @@ function mvc_install() {
  * Given a module name and a corresponding model name, this will load the model
  * from drupalgap.mvc.models.
  */
-function drupalgap_mvc_model_load(module, name) {
+function model_load(module, name) {
   try {
     if (drupalgap.settings.debug) {
-      console.log('drupalgap_mvc_model_load(' + module + ', ' + name + ')');
+      console.log('model_load(' + module + ', ' + name + ')');
     }
     if (drupalgap.mvc.models[module] === 'undefined' || drupalgap.mvc.models[module][name] === 'undefined') {
       return false;
@@ -181,7 +185,7 @@ function drupalgap_mvc_model_load(module, name) {
     return drupalgap.mvc.models[module][name];
   }
   catch (error) {
-    alert('drupalgap_mvc_model_load - ' + error);
+    alert('model_load - ' + error);
   }
 }
 
@@ -195,7 +199,7 @@ function drupalgap_mvc_model_create_form(module, type) {
       console.log('drupalgap_mvc_model_create_form(' + module + ', ' + type + ')');
     }
     var form = false;
-    var model = drupalgap_mvc_model_load(module, type);
+    var model = model_load(module, type);
     if (model) {
       form = {
         id:"drupalgap_mvc_model_create_form",
@@ -234,10 +238,9 @@ function drupalgap_mvc_model_create_form_submit(form, form_state) {
                  form_state.values.module + '/' +
                  form_state.values.type + '/' +
                  form_state.values.id;*/
-      var path = 'collection/' +
+      var path = 'collection/list/' +
                  form_state.values.module + '/' +
-                 form_state.values.type + '/list';
-      alert(path);
+                 form_state.values.type;
       drupalgap_goto(path);
     }
     else {
@@ -274,7 +277,7 @@ function item_save(item) {
     if (typeof item === 'undefined') { return false; }
     
     // Grab the settings for this collection.
-    var settings_key = drupalgap_mvc_get_collection_key('settings', item.module, item.type);
+    var settings_key = mvc_get_collection_key('settings', item.module, item.type);
     var settings = JSON.parse(window.localStorage.getItem(settings_key));
     
     // If there is no id, then this is a new item, grab the next id to use.
@@ -312,8 +315,12 @@ function item_save(item) {
 function item_view_page(module, type, item) {
   try {
     console.log(JSON.stringify(arguments));
-    alert('item_view_page');
-    return 'funk that page!';
+    var html = '';
+    var model = model_load(module, type);
+    if (model) {
+      html = theme('item', {"model":model, "item":item});
+    }    
+    return html;
   }
   catch (error) {
     alert('item_view_page - ' + error);
