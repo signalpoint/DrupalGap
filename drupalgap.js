650dc71f335f5e8076b51827f5cb238e4fbeb157
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2013-07-12 12:15:40 -0400

    Updated the comment description of drupalgap_item_list_populate().

--- drupalgap.js
+++ drupalgap.js
@@ -618,9 +618,9 @@ function drupalgap_includes_load() {
 }
 
 /**
- * Given an html list element id and an array of items, this will populate
- * the clear the list, populate it with the items, destroy the list and then
- * refresh the list with the new items.
+ * Given an html list element id and an array of items, this will clear the
+ * list, populate it with the items, destroy the list and then refresh the list
+ * with the new items.
  */
 function drupalgap_item_list_populate(list_css_selector, items) {
   try {
