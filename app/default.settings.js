44fefb91ffdd689ac3f7b6f3409f52c2cc5c11ef
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2013-08-14 22:43:49 -0400

    Fixed #134. Changed default.settings.js from using a giant JSON object into more manageable sections of code.

--- app/default.settings.js
+++ app/default.settings.js
@@ -1,154 +1,221 @@
-/**
- * Specify your DrupalGap's Mobile Application settings here. 
- */
-drupalgap.settings = {
-  
-  /* Paths */
-  'site_path':'', /* e.g. http://www.example.com */
-  'base_path':'/',
-  'default_services_endpoint':'drupalgap',
-  'clean_urls':false, /* set to true if you have clean urls enabled on your site */
-  
-  /* App Title */
-  'title':'DrupalGap',
-  
-  /* App Front Page */
-  'front':'dashboard',
-  
-  /* Language */
-  'language':'und',
-  
-  /* Files */
-  'file_public_path':'sites/default/files',
-  
-  /* Debug */
-  'debug':false, /* Set to true to see console.log debug information. Set to
-                   false when publishing app! */
-  'debug_level':0, /* 0 = mild, 1 = medium (), 2 = spicy () */
-  
-  /* Theme */
-  'theme':'easystreet3',
-  
-  /* Logo */
-  'logo':'themes/easystreet3/images/drupalgap.jpg',
-  
-  /* Cache Performance Settings */
-  cache:{
-    
-    /* Set to true to load the page.tpl.html contents from cache */
-    theme_registry:true,
-    
-    /* Allow entities retrieved from the Drupal server to be cached on the
-       device using local storage.  */
-    entity:{
-      enabled:true, /* Set to true to enable entity local storage caching. */
-      expiration:3600 /* Number of seconds before cached copy of entity expires. 
-                       Set to 0 to cache forever, set to 60 for one minute, etc.  */
-    },
+/* Specify DrupalGap Mobile Application Settings Here */
+drupalgap.settings = {};
 
+/***************|
+ * Drupal Paths |
+ ***************/
+ 
+// Site Path
+drupalgap.settings.site_path = ''; // e.g. http://www.example.com
+
+// Base Path
+drupalgap.settings.base_path = '/';
+
+// Default Services Endpoint Path
+drupalgap.settings.default_services_endpoint = 'drupalgap';
+
+// Public Files Directory Path
+drupalgap.settings.file_public_path = 'sites/default/files';
+
+// Clean URLs (optional)
+drupalgap.settings.clean_urls = false; // Setting to false is recommended.
+
+/*************|
+ * Appearance |
+ *************/
+
+// App Title
+drupalgap.settings.title = 'DrupalGap';
+ 
+// App Front Page
+drupalgap.settings.front = 'dashboard';
+
+// Theme
+drupalgap.settings.theme = 'easystreet3';
+
+// Logo
+drupalgap.settings.logo = 'themes/easystreet3/images/drupalgap.jpg';
+
+// Language
+drupalgap.settings.language = 'und';
+
+/*****************************************|
+ * Modules - http://drupalgap.org/node/74 |
+ *****************************************/
+
+// Contributed Modules - www/app/modules
+drupalgap.modules.contrib = [
+  /*{name:'example'},*/
+];
+
+// Custom Modules - www/app/modules/custom
+drupalgap.modules.custom = [
+  /*{name:'example'},*/
+];
+
+/****************************************|
+ * Blocks - http://drupalgap.org/node/83 |
+ ****************************************/
+drupalgap.settings.blocks = {};
+
+// Easy Street 3 Theme Blocks
+drupalgap.settings.blocks.easystreet3 = {
+  header:{
+    header:{}
   },
-  
-  /* Blocks */
-  'blocks':{
-    'easystreet3':{
-      'header':{
-        'header':{}
-      },
-      'navigation':{
-        'user_menu_anonymous':{
-          'roles':{
-            'value':['anonymous user'],
-            'mode':'include',
-          }
-        },
-        'user_menu_authenticated':{
-          'roles':{
-            'value':['authenticated user'],
-            'mode':'include',
-          }
-        }
-      },
-      'sub_navigation':{
-        'main_menu':{
-          'roles':{
-            'value':['administrator'],
-            'mode':'include',
-          }
-        },
-        'primary_local_tasks':{},
-      },
-      'content':{
-        'main':{}
-      },
-      'footer':{
-        'powered_by':{}
-      },
+  navigation:{
+    user_menu_anonymous:{
+      roles:{
+        value:['anonymous user'],
+        mode:'include',
+      }
     },
-  },
-  
-  /* Menus */
-  'menus':{
-    'main_menu':{
-      'links':[
-        {'title':'Content','path':'node','options':{'attributes':{'data-icon':'star'}}},
-        {'title':'Taxonomy','path':'taxonomy/vocabularies','options':{'attributes':{'data-icon':'grid'}}},
-        {'title':'Users','path':'user-listing','options':{'attributes':{'data-icon':'info'}}},
-      ],
-    },
-    'user_menu_anonymous':{
-      'links':[
-        {'title':'Login','path':'user/login'},
-        {'title':'Register','path':'user/register'},
-      ],
-    },
-    'user_menu_authenticated':{
-      'links':[
-        {'title':'My Account','path':'user'},
-        {'title':'Logout','path':'user/logout'},
-      ],
-    },
-    /* Region menu links. */
-    'regions':{
-      'header':{
-        'links':[
-          /* Home Button */
-          {
-            'title':'Home',
-            'path':'',
-            "options":{"attributes":{"data-icon":"home", "class":"ui-btn-left"}},
-            "pages":{
-              "value":[''],
-              "mode":"exclude",
-            }
-          },
-          /* Back Button */
-          {
-            'title':'Back',
-            "options":{
-              "attributes":{
-                "data-icon":"back",
-                "class":"ui-btn-right",
-                "onclick":"javascript:drupalgap_back();"
-              }
-            },
-            "pages":{
-              "value":[''],
-              "mode":"exclude",
-            }
-          }
-        ],
-      },
+    user_menu_authenticated:{
+      roles:{
+        value:['authenticated user'],
+        mode:'include',
+      }
     }
   },
+  sub_navigation:{
+    main_menu:{
+      roles:{
+        value:['administrator'],
+        mode:'include',
+      }
+    },
+    primary_local_tasks:{},
+  },
+  content:{
+    main:{}
+  },
+  footer:{
+    powered_by:{}
+  }
 };
 
-/* Custom Modules */
-drupalgap.modules.custom = [
-  /*{'name':'example'},*/
-];
+/***************************************|
+ * Menus - http://drupalgap.org/node/85 |
+ ***************************************/
+drupalgap.settings.menus = {};
 
-/* Contrib Modules */
-drupalgap.modules.contrib = [
-  /*{'name':'example'},*/
-];
+// User Menu Anonymous
+drupalgap.settings.menus['user_menu_anonymous'] = {
+  links:[
+    {title:'Login','path':'user/login'},
+    {title:'Register','path':'user/register'}
+  ]
+};
+
+// User Menu Authenticated
+drupalgap.settings.menus['user_menu_authenticated'] = {
+  links:[
+    {title:'My Account','path':'user'},
+    {title:'Logout','path':'user/logout'}
+  ]
+};
+
+// Main Menu
+drupalgap.settings.menus['main_menu'] = {
+  links:[
+    {
+      title:'Content',
+      path:'node',
+      options:{
+        attributes:{
+          'data-icon':'star'
+        }
+      }
+    },
+    {
+      title:'Taxonomy',
+      path:'taxonomy/vocabularies',
+      options:{
+        attributes:{
+          'data-icon':'grid'
+        }
+      }
+    },
+    {
+      title:'Users',
+      path:'user-listing',
+      options:{
+        attributes:{
+          'data-icon':'info'
+        }
+      }
+    }
+  ]
+};
+
+/****************************************************|
+ * Region Menu Links - http://drupalgap.org/node/173 |
+ ****************************************************/
+drupalgap.settings.menus.regions = {};
+
+// Header Region Links
+drupalgap.settings.menus.regions['header'] = {
+  links:[
+    /* Home Button */
+    {
+      title:'Home',
+      path:'',
+      options:{
+        attributes:{
+          "data-icon":"home",
+          "class":"ui-btn-left"
+        }
+      },
+      pages:{
+        value:[''],
+        mode:"exclude"
+      }
+    },
+    /* Back Button */
+    {
+      title:'Back',
+      options:{
+        attributes:{
+          "data-icon":"back",
+          "class":"ui-btn-right",
+          "onclick":"javascript:drupalgap_back();"
+        }
+      },
+      pages:{
+        value:[''],
+        mode:"exclude"
+      }
+    }
+  ]
+};
+
+/**************|
+ * Development |
+ **************/
+
+// Debug
+drupalgap.settings.debug = false; /* Set to true to see console.log debug
+                                     information. Set to false when publishing
+                                     app! */
+
+// Debug Level
+drupalgap.settings.debug_level = 0; /* 0 = mild, just a little debugging
+                                       1 = medium
+                                       2 = spicy, lots of debugging */
+
+/***********************|
+ * Performance Settings |
+ ***********************/
+drupalgap.settings.cache = {};
+
+// Theme Registry - Set to true to load the page.tpl.html contents from cache.
+drupalgap.settings.cache.theme_registry = true;
+
+// Entities - Allow entities retrieved from the Drupal server to be cached on
+//            the mobile device using local storage.
+drupalgap.settings.cache.entity = {
+  enabled:true,   /* Set to true to enable entity local storage caching. */
+  expiration:3600 /* Number of seconds before cached copy of entity expires. Set
+                     to 0 to cache forever, set to 60 for one minute, etc. */
+};
 
