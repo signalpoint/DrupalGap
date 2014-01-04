4f5669c0aad961933e895df826153060f249d2af
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-04 02:41:27 -0500

    Changed drupalgap_service_resource_extract_results() to only work on system connect, since we won't be doing any resource bundling on anything except system connect, also changed it to be JS based, dropped jQuery.

--- modules/services/services.js
+++ modules/services/services.js
@@ -18,29 +18,19 @@ function drupalgap_services_get_entity_resource(entity_type) {
  */
 function drupalgap_service_resource_extract_results(options) {
   try {
-    if (options.service == 'drupalgap_system' || options.service == 'drupalgap_user') {
-      if (options.resource == 'connect' || options.resource == 'login') {
-        // Depending on the service resource, extract the permissions
-        // from the options data.
-        var permissions = {};
-        if (options.service == 'drupalgap_system' && options.resource == 'connect') {
-          permissions = options.data.user_permissions;
-        }
-        else if (options.service == 'drupalgap_user' && options.resource == 'login') {
-          permissions = options.data.drupalgap_system_connect.user_permissions;
-        }
-        // Now iterate over the extracted user_permissions and attach to
-        // the drupalgap.user.permissions variable.
-        drupalgap.user.permissions = [];
-        $.each(permissions, function(index, object){
-          drupalgap.user.permissions.push(object.permission)
-        });
+    console.log('drupalgap_service_resource_extract_results');
+    console.log(options.data);
+    if (options.service == 'system' && options.resource == 'connect') {
+      var permissions = options.data.user_permissions;
+      for (var permission in permissions) {
+        Drupal.user.permissions.push(permissions[permission]);
       }
+      console.log('user_permissions');
+      console.log(Drupal.user.permissions);
     }
   }
   catch (error) {
-    alert('drupalgap_service_resource_extract_results - ' + error);
-    return null;
+    console.log('drupalgap_service_resource_extract_results - ' + error);
   }
 }
 
