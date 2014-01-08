5aaa07a93757d2b2f833500d73cd2868931434af
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-07 23:56:46 -0500

    Added views_datasource_get_view_result() and deprecated drupalgap.views_datasource.call().

--- src/modules/views_datasource/views_datasource.js
+++ src/modules/views_datasource/views_datasource.js
@@ -1,36 +1,51 @@
 /**
- * The DrupalGap Views Datasource JSON object. Used to fetch views_json results.
+ * Given a path to a Views Datasource (Views JSON) view, this will get the
+ * results and pass them along to the provided success callback.
+ * @param {String} path
+ * @param {Object} options
+ */
+function views_datasource_get_view_result(path, options) {
+  try {
+    Drupal.services.call({
+        endpoint: '',
+        service: 'views_datasource',
+        resource: '',
+        method: 'GET',
+        path: path,
+        success: function(result) {
+          try {
+            if (options.success) { options.success(result); }
+          }
+          catch (error) {
+            console.log(
+              'views_datasource_get_view_result - success - ' + error
+            );
+          }
+        },
+        error: function(xhr, status, message) {
+          try {
+            if (options.error) { options.error(xhr, status, message); }
+          }
+          catch (error) { console.log('user_register - error - ' + error); }
+        }
+    });
+  }
+  catch (error) { console.log('views_datasource_get_view_result - ' + error); }
+}
+
+/**
+ * @deprecated - views_datasource_get_view_result() instead.
  */
 drupalgap.views_datasource = {
   'options': { /* these are set by drupalgap_api_default_options() */ },
   'call': function(options) {
     try {
-      alert('views_datasource - call not implemented yet!');
-      return false;
-
-      if (!options.path) {
-        navigator.notification.alert(
-          'No path provided!',
-          function() {},
-          'DrupalGap Views Datasource Error',
-          'OK'
-        );
-        return;
-      }
-      var api_options =
-        drupalgap_chain_callbacks(drupalgap.views_datasource.options, options);
-      api_options.endpoint = '';
-      api_options.path = options.path;
-      drupalgap.api.call(api_options);
+      var msg = 'WARNING: drupalgap.views_datasource has been deprecated! ' +
+      'Use views_datasource_get_view_result() instead.';
+      console.log(msg);
+      views_datasource_get_view_result(options.path, options);
     }
-    catch (error) {
-      navigator.notification.alert(
-        error,
-        function() {},
-        'DrupalGap Views Datasource Error',
-        'OK'
-      );
-    }
+    catch (error) { console.log('drupalgap.views_datasource - ' + error); }
   }
 };
 
