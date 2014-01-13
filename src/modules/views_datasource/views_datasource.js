231e7682f9faef9b3a08ed50a602d69a8c806555
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-13 13:48:27 -0500

    Added a local storage layer to views_datasource_get_view_result().

--- src/modules/views_datasource/views_datasource.js
+++ src/modules/views_datasource/views_datasource.js
@@ -6,6 +6,35 @@
  */
 function views_datasource_get_view_result(path, options) {
   try {
+    // If local storage caching is enabled, let's see if we can load the results
+    // from there. If we successfully loaded the result, make sure it didn't
+    // expire. If it did expire, remove it from local storage. If we don't have
+    // it in local storage, or local storage caching is disabled, call Drupal to
+    // get the results. After the results are fetched, save them to local
+    // storage with an expiration time, if necessary. We use the path of the
+    // view as the local storage key.
+    // If we are resetting, remove the item from localStorage.
+    if (options.reset) { window.localStorage.removeItem(path); }
+    else if (Drupal.settings.cache.views.enabled) {
+      var result = window.localStorage.getItem(path);
+      if (result) {
+        // Loaded from local storage, did it expire?
+        dpm(result);
+        result = JSON.parse(result);
+        if (typeof result.expiration !== 'undefined' &&
+          result.expiration != 0 &&
+          time() > result.expiration
+        ) {
+          // Expired, remove from local storage.
+          window.localStorage.removeItem(path);
+        }
+        else if (options.success) {
+          // Did not expire yet, use it.
+          options.success(result);
+          return;
+        }
+      }
+    }
     Drupal.services.call({
         endpoint: '',
         service: 'views_datasource',
@@ -14,7 +43,19 @@ function views_datasource_get_view_result(path, options) {
         path: path,
         success: function(result) {
           try {
-            if (options.success) { options.success(result); }
+            if (options.success) {
+              if (Drupal.settings.cache.views.enabled) {
+                var expiration =
+                  time() + Drupal.settings.cache.views.expiration;
+                if (Drupal.settings.cache.views.expiration == 0) {
+                  expiration = 0;
+                }
+                // Saving to local storage.
+                result.expiration = expiration;
+                window.localStorage.setItem(path, JSON.stringify(result));
+              }
+              options.success(result);
+            }
           }
           catch (error) {
             console.log(
@@ -26,7 +67,9 @@ function views_datasource_get_view_result(path, options) {
           try {
             if (options.error) { options.error(xhr, status, message); }
           }
-          catch (error) { console.log('user_register - error - ' + error); }
+          catch (error) {
+            console.log('views_datasource_get_view_result - error - ' + error);
+          }
         }
     });
   }
