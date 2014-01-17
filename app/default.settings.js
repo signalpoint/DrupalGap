b699d3e4ad67ce469c6b2eee50efece014ddc671
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-01-16 22:21:33 -0500

    Added note to make module loading easier.

--- app/default.settings.js
+++ app/default.settings.js
@@ -66,12 +66,14 @@ drupalgap.settings.loading = {
 // E.g., these 2 lines will load the module at www/app/modules/example
 //var example_contrib_module = module_object_template('example');
 //Drupal.modules.contrib.example = example_contrib_module;
+// @todo - make this an easy one liner, e.g. module_load('contrib', 'date', {...});
 
 // Custom Modules - www/app/modules/custom
 
 // E.g., these 2 lines will load the module at www/app/modules/custom/example
 //var example_custom_module = module_object_template('example');
 //Drupal.modules.custom.example = example_custom_module;
+// @todo - make this an easy one liner, e.g. module_load('custom', 'my_module', {...});
 
 /***************************************|
  * Menus - http://drupalgap.org/node/85 |
