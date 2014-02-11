c6fe30696c7b3714f1aae87264e298a2edd00995
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-02-11 11:43:07 -0500

    Added drupalgap_alert().

--- src/drupalgap.js
+++ src/drupalgap.js
@@ -449,6 +449,34 @@ function drupalgap_add_css() {
 }
 
 /**
+ * Alerts a message to the user using PhoneGap's alert. It is important to
+ * understand this is an async function, so code will continue to execute while
+ * the alert is displayed to the user.
+ * You may optionally pass in a second argument as a JSON object with the
+ * following properties:
+ *   alertCallback - the function to call after the user presses OK
+ *   title - the title to use on the alert box, defaults to 'Alert'
+ *   buttonName - the text to place on the button, default to 'OK'
+ * @param {String} message
+ */
+function drupalgap_alert(message) {
+  try {
+    var options = null;
+    if (arguments[1]) { options = arguments[1]; }
+    var alertCallback = function() { };
+    var title = 'Alert';
+    var buttonName = 'OK';
+    if (options) {
+      if (options.alertCallback) { alertCallback = options.alertCallback; }
+      if (options.title) { title = options.title; }
+      if (options.buttonName) { title = options.buttonName; }
+    }
+    navigator.notification.alert(message, alertCallback, title, buttonName);
+  }
+  catch (error) { console.log('drupalgap_alert - ' + error); }
+}
+
+/**
  * Rounds up all blocks defined by hook_block_info and places them in the
  * drupalgap.blocks array.
  */
