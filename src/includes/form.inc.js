4d5eb709d21a557d16f13c605f5d5e97b58d2af3
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2014-04-30 11:30:12 -0400

    Fix #70. Change _drupalgap_form_render_elements() to pay attention to element weights on fields, and info extra fields.

--- src/includes/form.inc.js
+++ src/includes/form.inc.js
@@ -580,21 +580,37 @@ function _drupalgap_form_render_elements(form) {
     var content = '';
     var content_weighted = [];
     // For each form element, if the element objects name property isn't set,
-    // set it, then render the element if access is permitted.
+    // set it, then render the element if access is permitted. While rendering
+    // the elements, set them aside according to their widget weight so they
+    // can be appended to the content string in the correct order later.
     $.each(form.elements, function(name, element) {
       if (!element.name) { element.name = name; }
       if (drupalgap_form_element_access(element)) {
-        if (!empty(element.field_info_instance) &&
-            !empty(element.field_info_instance.widget) &&
-            !empty(element.field_info_instance.widget.weight)) {
-              content_weighted[element.field_info_instance.widget.weight] =
-                _drupalgap_form_render_element(form, element);
-        } else {
-          content += _drupalgap_form_render_element(form, element);
+        if (element.is_field && element.field_info_instance.widget.weight) {
+          content_weighted[element.field_info_instance.widget.weight] =
+            _drupalgap_form_render_element(form, element);
+        }
+        else {
+          // This is not a field, if it has a weight in field_info_extra_fields
+          // use it, otherwise just append it to the content.
+          if (
+            form.entity_type && form.bundle &&
+            typeof drupalgap.field_info_extra_fields[form.bundle][name] !==
+              'undefined' &&
+            typeof
+              drupalgap.field_info_extra_fields[form.bundle][name].weight !==
+              'undefined'
+          ) {
+            var weight =
+              drupalgap.field_info_extra_fields[form.bundle][name].weight;
+            content_weighted[weight] =
+            _drupalgap_form_render_element(form, element);
+          }
+          else { content += _drupalgap_form_render_element(form, element); }
         }
       }
      });
-    // add the weighted fields to the mix
+    // Prepend the weighted elements to the content.
     if (!empty(content_weighted)) {
       content = content_weighted.join('\n') + content;
     }
