42870db2960f135b7798f56df5c81a9ab4a9dae8
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-04-13 23:43:24 -0400

    Fix #530.

--- src/modules/entity/entity.js
+++ src/modules/entity/entity.js
@@ -92,8 +92,10 @@ function drupalgap_entity_edit_form_delete_confirmation(entity_type,
         window.localStorage.removeItem(
           entity_local_storage_key(entity_type, entity_id)
         );
-        // Go to the front page.
-        drupalgap_goto('', { reloadPage: true, form_submission: true });
+        // Go to the front page, unless a form action path was specified.
+        var form = drupalgap_form_local_storage_load('node_edit');
+        var destination = form.action ? form.action : '';
+        drupalgap_goto('', { destination: true, form_submission: true });
       };
       // Call the delete function.
       var name = services_get_resource_function_for_entity(
