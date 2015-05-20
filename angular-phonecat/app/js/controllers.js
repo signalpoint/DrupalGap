e3029b0203e47c1bc618063b4f8b26b83746df65
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-05-20 17:46:57 -0400

    Add controllers.js.

---
+++ angular-phonecat/app/js/controllers.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/* Controllers */
+
+var phonecatControllers = angular.module('phonecatControllers', []);
+
+/*phonecatControllers.controller('dgResolveController', ['$scope',
+  function($scope) {
+    $scope.templateUrl = 'js/drupalgap/src/modules/user/views/user_login_form.html'
+  }]);*/
+
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+  }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+  function($scope, $routeParams, Phone) {
+    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      $scope.mainImageUrl = phone.images[0];
+    });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    }
+  }]);
