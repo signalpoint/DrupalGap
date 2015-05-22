2439eeb2963c93be2d62f9376d133965c33d9507
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-05-22 03:43:21 -0400

    Add stuff pulled out of drupalgap_services_request_pre_postprocess_alter() and placed it into drupalgap_service_resource_extract_results().

--- src/modules/services/services.js
+++ src/modules/services/services.js
@@ -50,15 +50,26 @@ function services_get_resource_function_for_entity(entity_type, crud) {
 function drupalgap_service_resource_extract_results(options) {
   try {
     if (options.service == 'system' && options.resource == 'connect') {
+      drupalgap.remote_addr = options.data.remote_addr;
+      drupalgap.entity_info = options.data.entity_info;
+      drupalgap.field_info_instances = options.data.field_info_instances;
+      drupalgap.field_info_fields = options.data.field_info_fields;
+      drupalgap.field_info_extra_fields = options.data.field_info_extra_fields;
+      // @TODO uncomment once 7.x-2.x is more stable
+      /*drupalgap.taxonomy_vocabularies =
+        drupalgap_taxonomy_vocabularies_extract(
+          result.taxonomy_vocabularies
+        );*/
+
       // The system connect resource's success function places what is in
       // options.data.user to overwrite Drupal.user, so anything we want in
       // the Drupal.user object must be added to options.data.user isntead.
       // Extract and build the user's permissions.
-      options.data.user.permissions = [];
+      /*options.data.user.permissions = [];
       var permissions = options.data.user_permissions;
       for (var permission in permissions) {
         options.data.user.permissions.push(permissions[permission]);
-      }
+      }*/
       // Pull out the content types, and set them by their type.
       var content_types_list = options.data.content_types_list;
       for (var index in content_types_list) {
@@ -67,8 +78,8 @@ function drupalgap_service_resource_extract_results(options) {
           drupalgap.content_types_list[object.type] = object;
       }
       // Pull out the content types user permissions.
-      options.data.user.content_types_user_permissions =
-        options.data.content_types_user_permissions;
+      /*options.data.user.content_types_user_permissions =
+        options.data.content_types_user_permissions;*/
       // Pull out the site settings.
       drupalgap.site_settings = options.data.site_settings;
       // Pull out the date formats and types.
