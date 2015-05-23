af6e032cf98b3b0bfebce6cc4203727754eb2694
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-05-23 03:59:28 -0400

    Add drupalgap_field_display(). Add hook_field_formatter_view() and hook_field_widget_form() controllers and deprecate drupalgap_field_info_instances_add_to_form().

--- src/modules/field/field.js
+++ src/modules/field/field.js
@@ -1,3 +1,239 @@
+phonecatControllers.controller('hook_field_formatter_view',
+  [ '$scope', '$element', function($scope, $element) {
+    try {
+
+      dpm('hook_field_formatter_view');
+      //console.log(arguments);
+      
+      var content = '';
+      
+      var entity_type = $scope.$parent.entity_type;
+      var entity = $scope.$parent[entity_type];
+      var bundle = drupalgap_get_bundle(entity_type, entity);
+      var field_name = $element.attr('field_name');
+      var field = drupalgap_field_info_instance(entity_type, field_name, bundle);
+      var display = drupalgap_field_display(field);
+      
+      //console.log(entity_type);
+      //console.log(entity);
+      //console.log(field_name);
+      //console.log(field);
+      //console.log(display);
+      
+      // Determine module that implements the hook_field_formatter_view,
+      // then determine the hook's function name, then render the field content.
+      // If there wasn't a module specified in the display, look to the module
+      // specified in the field widget. If we still don't find it, then just
+      // return.
+      var module = display['module'];
+      if (!module) {
+        if (!field.widget.module) {
+          var msg = 'hook_field_formatter_view - ' +
+            'unable to locate the module for the field (' + field_name + ')';
+          console.log(msg);
+          return content;
+        }
+        else { module = field.widget.module; }
+      }
+      var function_name = module + '_field_formatter_view';
+      if (!drupalgap_function_exists(function_name)) {
+        console.log(
+          'WARNING: hook_field_formatter_view - ' + function_name + '() ' +
+          'does not exist! (' + field_name + ')'
+        );
+        return;
+      }
+      
+      // Grab the field formatter function, then grab the field items
+      // from the entity, then call the formatter function and append its result
+      // to the entity's content.
+      var fn = window[function_name];
+      var items = null;
+
+      // Check to see if translated content based on app's language setting
+      // is present or not. If yes, then use that language as per setting.
+      // Determine the language code. Note, multi lingual sites may have a
+      // language code on the entity, but still have 'und' on the field, so
+      // fall back to 'und' if the field's language code doesn't match the
+      // entity's language code.
+      var default_lang = language_default();
+      var language = entity.language;
+      if (entity[field_name]) {
+        if (entity[field_name][default_lang]) {
+          items = entity[field_name][default_lang];
+        }
+        else if (entity[field_name][language]) {
+          items = entity[field_name][language];
+        }
+        else if (entity[field_name]['und']) {
+          items = entity[field_name]['und'];
+          language = 'und';
+        }
+        else { items = entity[field_name]; }
+      }
+
+      // @TODO - We've been sending 'field' as the instance
+      // (drupalgap_field_info_instance), and the 'instance' as the field
+      // (drupalgap_field_info_field). This is backwards, and should be
+      // reversed. All contrib modules with field support will need to be
+      // udpated to reflect this. Lame.
+      var elements = fn(
+        entity_type,
+        entity,
+        field, /* This is actually the instance, doh! (I think) */
+        drupalgap_field_info_field(field_name),
+        language,
+        items,
+        display
+      );
+      for (var delta in elements) {
+          if (!elements.hasOwnProperty(delta)) { continue; }
+          var element = elements[delta];
+          // If the element has markup, render it as is, if it is
+          // themeable, then theme it.
+          var element_content = '';
+          if (element.markup) { element_content = element.markup; }
+          else if (element.theme) {
+            element_content = theme(element.theme, element);
+          }
+          content += element_content;
+      }
+      
+      // Render the field label, if necessary.
+      if (content != '' && display['label'] != 'hidden') {
+        var label = '<h3>' + field.label + '</h3>';
+        // Place the label above or below the field content.
+        label = '<div>' + label + '</div>';
+        switch (display['label']) {
+          case 'below':
+            content += label;
+            break;
+          case 'above':
+          default:
+            content = label + content;
+            break;
+        }
+      }
+      
+      $scope[field_name] = content;
+
+    }
+    catch (error) { console.log('hook_field_formatter_view - ' + error); }
+  } ]
+);
+
+phonecatControllers.controller('hook_field_widget_form',
+  [ '$scope', '$element', function($scope, $element) {
+    try {
+
+      dpm('hook_field_widget_form');
+      //console.log(arguments);
+      
+      var entity_type = $scope.$parent.entity_type;
+      var entity = $scope.$parent[entity_type];
+      var bundle = drupalgap_get_bundle(entity_type, entity);
+      var form = $scope.$parent.form;
+      var field_name = $element.attr('field_name');
+      
+      //console.log(entity_type);
+      //console.log(entity);
+      //console.log(bundle);
+      //console.log(form);
+      //console.log(field_name);
+      
+      // Load the field instance.
+      var field = drupalgap_field_info_instance(entity_type, field_name, bundle);
+      console.log(field);
+      
+      // The user registration form is a special case, in that we only want
+      // to place fields that are set to display on the user registration
+      // form. Skip any fields not set to display.
+      if (form.id == 'user_register_form' &&
+        !field.settings.user_register_form) {
+        return;
+      }
+      var field_info = drupalgap_field_info_field(name);
+      if (field_info) {
+        form.elements[name] = {
+          type: field_info.type,
+          title: field.label,
+          required: field.required,
+          description: field.description
+        };
+        if (!form.elements[name][language]) {
+          form.elements[name][language] = {};
+        }
+        var default_value = field.default_value;
+        var delta = 0;
+        var cardinality = parseInt(field_info.cardinality);
+        if (cardinality == -1) {
+          cardinality = 1; // we'll just add one element for now, until we
+                           // figure out how to handle the 'add another
+                           // item' feature.
+        }
+        if (entity && entity[name] && entity[name].length != 0) {
+          for (var delta = 0; delta < cardinality; delta++) {
+            // @TODO - is this where we need to use the idea of the
+            // value_callback property present in Drupal's FAPI? That way
+            // each element knows how to map the entity data to its element
+            // value property.
+            if (
+              entity[name][language][delta] &&
+              typeof entity[name][language][delta].value !== 'undefined'
+            ) { default_value = entity[name][language][delta].value; }
+            // If the default_value is null, set it to an empty string.
+            if (default_value == null) { default_value = ''; }
+            // @todo - It appears not all fields have a language code to use
+            // here, for example taxonomy term reference fields don't!
+            form.elements[name][language][delta] = {
+              value: default_value
+            };
+            // Place the field item onto the element.
+            if (entity[name][language][delta]) {
+              form.elements[name][language][delta].item =
+                entity[name][language][delta];
+            }
+          }
+        }
+
+        // Give module's a chance to alter their own element during the form
+        // build, that way element properties will be saved to local storage
+        // and then available during hook_field_widget_form() and the form
+        // submission process.
+        var fn = field.widget.module + '_field_info_instance_add_to_form';
+        if (drupalgap_function_exists(fn)) {
+          window[fn](entity_type, bundle, form, entity, form.elements[name]);
+        }
+
+      }
+
+    }
+    catch (error) { console.log('hook_field_widget_form - ' + error); }
+  } ]
+);
+
+/**
+ * Given a field, this will return its display.
+ * @param {Object} field
+ * @return {Object}
+ */
+function drupalgap_field_display(field) {
+  try {
+    var display = field.display['default'];
+    if (field.display['drupalgap']) {
+      display = field.display['drupalgap'];
+      // If a module isn't listed on the drupalgap display, use the default
+      // display's module.
+      if (
+        typeof display.module === 'undefined' &&
+        typeof field.display['default'].module !== 'undefined'
+      ) { display.module = field.display['default'].module; }
+    }
+    return display;
+  }
+  catch (error) { console.log('drupalgap_field_display - ' + error); }
+}
+
 /**
  * Given a field name, this will return its field info.
  * @param {String} field_name
@@ -97,10 +333,13 @@ function drupalgap_field_info_instances(entity_type, bundle_name) {
  * @param {String} bundle
  * @param {Object} form
  * @param {Object} entity
+ * @deprecated
  */
 function drupalgap_field_info_instances_add_to_form(entity_type, bundle,
   form, entity) {
   try {
+    console.log('DEPRECATED - drupalgap_field_info_instances_add_to_form()');
+    return;
     // Grab the field info instances for this entity type and bundle.
     var fields = drupalgap_field_info_instances(entity_type, bundle);
     // If there is no bundle, pull the fields out of the wrapper.
