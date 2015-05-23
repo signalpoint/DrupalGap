d265a599c6d7c91fa1c7d262114229dd0a2f8e4c
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-05-23 04:00:28 -0400

    Add 'view' and 'edit' modes when generating view templates for each entity type.

--- angular-phonecat/app/js/app.js
+++ angular-phonecat/app/js/app.js
@@ -34,15 +34,13 @@ var phonecatApp = angular.module('phonecatApp', [
   for (var entity_type in drupalgap.field_info_instances) {
     if (!drupalgap.field_info_instances.hasOwnProperty(entity_type)) { continue; }
     var entity_bundles = drupalgap.field_info_instances[entity_type];
-    //dpm(entity_type);
-    //console.log(entity_bundles);
     
+    // Initialize a place for this entity type's view(s) templates.
     drupalgap.views.templates[entity_type] = {};
     
     // For each bundle on the entity type...
     for (var bundle in entity_bundles) {
       if (!entity_bundles.hasOwnProperty(bundle)) { continue; }
-      //dpm(bundle);
       
       // Grab the field instances for this entity type and bundle.
       var instances = entity_bundles[bundle];
@@ -54,10 +52,25 @@ var phonecatApp = angular.module('phonecatApp', [
         continue;
       }
       
-      drupalgap.views.templates[entity_type][bundle] = {
-        template: '<div ng-model="' + entity_type + '">{{' + entity_type + '.' + entity_primary_key_title(entity_type) + '}}</div>'
-      };
-      
+      // Make a view template for each entity mode.
+      drupalgap.views.templates[entity_type][bundle] = { };
+      var modes = ['view', 'edit'];
+      for (var i = 0; i < modes.length; i++) {
+        var mode = modes[i];
+        var template = '';
+        switch (mode) {
+        case 'view':
+          template = '{{' + entity_type + '.' + entity_primary_key_title(entity_type) + '}}';
+          break;
+        case 'edit':
+          template = '{{' + entity_type + '.' + entity_primary_key_title(entity_type) + '}}';
+          break;
+        }
+        drupalgap.views.templates[entity_type][bundle][mode] = {
+          template: '<div ng-model="' + entity_type + '">' + template + '</div>'
+        };
+      }
+
       var field_weights = {};
       var field_displays = {};
       
@@ -69,17 +82,7 @@ var phonecatApp = angular.module('phonecatApp', [
         
         // Determine which display mode to use. The default mode will be used
         // if the drupalgap display mode is not present.
-        if (!field.display) { break; }
-        var display = field.display['default'];
-        if (field.display['drupalgap']) {
-          display = field.display['drupalgap'];
-          // If a module isn't listed on the drupalgap display, use the default
-          // display's module.
-          if (
-            typeof display.module === 'undefined' &&
-            typeof field.display['default'].module !== 'undefined'
-          ) { display.module = field.display['default'].module; }
-        }
+        var display = drupalgap_field_display(field);
         
         // Skip hidden fields.
         if (display.type == 'hidden') { continue; }
@@ -116,7 +119,20 @@ var phonecatApp = angular.module('phonecatApp', [
               if (target_weight == weight) {
                 if (completed_fields.indexOf(field_name) == -1) {
                   completed_fields.push(field_name);
-                  drupalgap.views.templates[entity_type][bundle].template += '<p>' + field_name + '</p>';
+                  
+                  var modes = ['view', 'edit'];
+                  for (var i = 0; i < modes.length; i++) {
+                    var mode = modes[i];
+                    var hook_name = mode == 'view' ?
+                      'hook_field_formatter_view' : 'hook_field_widget_form'
+                    var attrs = drupalgap_attributes({
+                      'ng-controller': hook_name,
+                      field_name: field_name
+                    });
+                    drupalgap.views.templates[entity_type][bundle][mode].template +=
+                      '<div ' + attrs + '>{{' + field_name + '}}</div>';
+                  }
+                  
                   /*entity.content += drupalgap_entity_render_field(
                     entity_type,
                     entity,
@@ -130,7 +146,8 @@ var phonecatApp = angular.module('phonecatApp', [
           }
       }
       
-      //console.log(completed_fields);
+      //console.log('views templates');
+      //console.log(drupalgap.views.templates);
 
     }
 
@@ -139,7 +156,7 @@ var phonecatApp = angular.module('phonecatApp', [
 }).run(['$rootScope', '$routeParams', '$location', function($rootScope, $routeParams, $location) {
 
       dpm('phonecatApp.run()');
-      console.log(arguments);
+      //console.log(arguments);
 
       drupalgap_ng_set('routeParams', $routeParams);
       drupalgap_ng_set('location', $location);
@@ -176,89 +193,49 @@ angular.module('jdrupal-ng').config(function($provide) {
 // CONFIGURE HOOK_MENU() ITEMS AS ANGULAR ROUTES
 phonecatApp.config(['$routeProvider',
   function($routeProvider) {
-    
-    dpm('config() - route provider...');
+
+    dpm('config() - building route provider...');
     //console.log(arguments);
-    
+
     // Attach hoom_menu() paths to Angular's routeProvider.
     for (var path in drupalgap.menu_links) {
       if (!drupalgap.menu_links.hasOwnProperty(path)) { continue; }
-      
+
       // Extract the menu link.
       var menu_link = drupalgap.menu_links[path];
-      
+
       // Skip 'MENU_DEFAULT_LOCAL_TASK' items.
       if (menu_link.type == 'MENU_DEFAULT_LOCAL_TASK') {
         console.log('WARNING: phonecatApp - deprecated | MENU_DEFAULT_LOCAL_TASK on path: ' + path);
         continue;
       }
-      
+
       // Use the active theme's page template as the templateUrl, if one wasn't
       // provided.
       if (!menu_link.templateUrl) {
         menu_link.templateUrl = 'js/drupalgap/themes/spi/page.tpl.html';
       }
-      
+
       // Determine the menu link's controller, by first falling back to the
       // page_callback property which we'll warn the developer about, otherwise
       // we'll use menu_link object for the routeProvider.
-      /*if (!menu_link.controller && menu_link.page_callback) {
-        console.log('DEPRECATED: phonecatApp - routeProvider | rename the ' + path + ' menu link path\'s page_callback property name to controller');
-        menu_link.controller = menu_link.page_callback;
-      }*/
       if (!menu_link.controller && menu_link.page_callback) {
         menu_link.controller = 'drupalgap_goto_controller';
-        // @WARNING - dynamically resolving page arguments always gets stuck
-        // with the last menu link item in this loop...
-        /*menu_link.resolve = {
-          menu_link: function() {
-            dpm('grabbing the menu link now!');
-            console.log(menu_link);
-            return menu_link.page_callback;
-          }
-        };*/
       }
       else if (!menu_link.controller) {
         console.log('WARNING: phonecatApp - routeProvider | no controller provided for path: ' + path);
       }
-      
-      // Warn the developer that each page_argument must be converted to an
-      // Angular "resolve".
-      /*var page_arguments = [];
-      if (typeof menu_link.page_arguments !== 'undefined') {
-        console.log('UPGRADE: phonecatApp - routeProvider | convert the page_arguments into a "resolve" on path: ' + path);
-      }*/
-      
+
       // Add the menu link to Angular's routeProvider.
       // @TODO apparently attaching all the controllers at once, instead of on
       // demand is expensive, seek alternative routes in utilizing controllers.
       //console.log(menu_link);
       // @TODO by attaching the complete hook_menu item link here, DG stuff ends
       // up in the same object as an Angular route, and we may or may not be
-      // colliding with how they do things.
+      // colliding with how Angular does things.
       $routeProvider.when('/' + path, menu_link);
+
     }
-
-    /* each page_argument can be dynamically resolved here!!! */
-    /*$routeProvider.when('/user/login', {
-        templateUrl: 'js/drupalgap/src/modules/user/views/user_login_form.html',
-        controller: 'user_login_form',
-        resolve: {
-          form: function($q, $timeout) {
-            return drupalgap_form_load('user_login_form');
-          }
-        }
-    });*/
-
-    // @TODO - we can't be setting the path and router path like this, because
-    // if we start on something other than the front page (i.e. a hash is in the
-    // url), this will set off incorrect values.
-    
-    // Set the app's front page on the routeProvider.
-    //drupalgap_path_set(drupalgap.settings.front);
-
-    // Determine and set the drupalgap router path.
-    //drupalgap_router_path_set(drupalgap_get_menu_link_router_path(drupalgap.settings.front));
 
     // Set the otherwise ruling to load the front page.
     $routeProvider.otherwise({
