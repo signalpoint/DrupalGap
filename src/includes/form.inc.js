8c617f34fd6079091991209db42b34098abaccc0
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-05-23 23:25:53 -0400

    Re-arrange functions.

--- src/includes/form.inc.js
+++ src/includes/form.inc.js
@@ -2,90 +2,12 @@
 phonecatControllers.controller('drupalgapFormController', ['$scope',
   function($scope) {
     dpm('drupalgapFormController');
+    console.log(arguments);
     // Create an empty form_state and attach the default submit handler to the
     // scope, if they haven't been set already.
     if (!$scope.form_state) { $scope.form_state = { values: { } } };
     if (!$scope.submit) { $scope.submit = _drupalgap_form_submit; }
   }]);
-
-/**
- * Internal function used to dynamically add another element item to a form for
- * unlimited value fields.
- * @param {String} form_id
- * @param {String} name
- * @param {Number} delta
- */
-function _drupalgap_form_add_another_item(form_id, name, delta) {
-  try {
-    // Locate the last item, load the form, extract the element from
-    // the form, generate default variables for the new item, determine the next
-    // delta value.
-    var selector = '.' + drupalgap_form_get_element_container_class(name) +
-      ' .drupalgap_form_add_another_item';
-    var add_another_item_button = $(selector);
-    var form = drupalgap_form_local_storage_load(form_id);
-    var language = language_default();
-    var item = drupalgap_form_element_item_create(
-      name,
-      form,
-      language,
-      delta + 1
-    );
-    form.elements[name][language][delta + 1] = item;
-    var element = form.elements[name];
-    var variables = {
-      attributes: {},
-      field_info_field: element.field_info_field,
-      field_info_instance: element.field_info_instance
-    };
-    var field_widget_form_function =
-      element.field_info_instance.widget.module + '_field_widget_form';
-    window[field_widget_form_function].apply(
-      null,
-      _drupalgap_form_element_items_widget_arguments(
-        form,
-        null,
-        element,
-        language,
-        delta + 1
-      )
-    );
-    drupalgap_form_local_storage_save(form);
-    $(add_another_item_button).before(
-      _drupalgap_form_render_element_item(form, element, variables, item)
-    );
-  }
-  catch (error) { console.log('_drupalgap_form_add_another_item - ' + error); }
-}
-
-/**
- * Returns a 'Cancel' button object that can be used on most forms.
- * @return {Object}
- */
-function drupalgap_form_cancel_button() {
-    return {
-      'title': t('Cancel'),
-      attributes: {
-        onclick: 'javascript:drupalgap_back();'
-      }
-    };
-}
-
-/**
- * Given a jQuery selector to a form, this will clear all the elements on
- * the UI.
- * @see http://stackoverflow.com/a/6364313/763010
- */
-function drupalgap_form_clear(form_selector) {
-  try {
-    $(':input', form_selector)
-     .not(':button, :submit, :reset, :hidden')
-     .val('')
-     .removeAttr('checked')
-     .removeAttr('selected');
-  }
-  catch (error) { console.log('drupalgap_form_clear - ' + error); }
-}
 
 /**
  * Given a form id, this will assemble and return the default form JSON object.
@@ -126,72 +48,6 @@ function drupalgap_form_defaults(form_id) {
   }
   catch (error) { console.log('drupalgap_form_defaults - ' + error); }
 }
-
-/**
- * Given a drupalgap form, this renders the form html and returns it.
- * @param {Object} form
- * @return {String}
- */
-function drupalgap_form_render(form) {
-  try {
-    // @TODO - we may possibly colliding html element ids!!! For example, I
-    // think the node edit page gets an id of "node_edit" and possibly so does
-    // the node edit form, which also may get an id of "node_edit". We may want
-    // to prefix both the template page and form ids with prefixes, e.g.
-    // drupalgap_page_* and drupalgap_form_*, but adding these prefixes could
-    // get annoying for css selectors used in jQuery and CSS. What to do?
-
-    // If no form id is provided, warn the user.
-    if (!form.id) {
-      return '<p>drupalgap_form_render() - missing form id!</p>' +
-        JSON.stringify(form);
-    }
-    // If the form already exists in the DOM, remove it.
-    if ($('form#' + form.id).length) { $('form#' + form.id).remove(); }
-    // Render the prefix and suffix and wrap them in their own div.
-    var prefix = form.prefix;
-    if (!empty(prefix)) {
-      prefix = '<div class="form_prefix">' + prefix + '</div>';
-    }
-    var suffix = form.suffix;
-    if (!empty(suffix)) {
-      suffix = '<div class="form_suffix">' + suffix + '</div>';
-    }
-    // Render the form's input elements.
-    var form_elements = _drupalgap_form_render_elements(form);
-    var form_attributes = drupalgap_attributes(form.options.attributes);
-    // Return the form html.
-    var form_html = '<div ng-controller="drupalgapFormController"><form ' + form_attributes + '>' +
-      prefix +
-      '<div id="drupalgap_form_errors"></div>' +
-      form_elements +
-      suffix +
-    '</form></div>';
-    return form_html;
-  }
-  catch (error) { console.log('drupalgap_form_render - ' + error); }
-}
-
-/**
- * Given a form element name and an error message, this attaches the error
- * message to the drupalgap.form_errors array, keyed by the form element name.
- * @param {String} name
- * @param {String} message
- */
-function drupalgap_form_set_error(name, message) {
-  try {
-    drupalgap.form_errors[name] = message;
-  }
-  catch (error) { console.log('drupalgap_form_set_error - ' + error); }
-}
-
-/*phonecatControllers.controller('drupalgap_get_form_controller', ['$scope', '$sce', 'form_id',
-  function($scope, $sce, form_id) {
-    try {
-      $scope.content = $sce.trustAsHtml(drupalgap_get_form(form_id));
-    }
-    catch (error) { console.log('drupalgap_get_form_controller - ' + error); }
-  }]);*/
 
 /**
  * Given a form id, this will render the form and return the html for the form.
@@ -372,6 +228,64 @@ function drupalgap_form_load(form_id) {
 }
 
 /**
+ * Given a drupalgap form, this renders the form html and returns it.
+ * @param {Object} form
+ * @return {String}
+ */
+function drupalgap_form_render(form) {
+  try {
+    // @TODO - we may possibly colliding html element ids!!! For example, I
+    // think the node edit page gets an id of "node_edit" and possibly so does
+    // the node edit form, which also may get an id of "node_edit". We may want
+    // to prefix both the template page and form ids with prefixes, e.g.
+    // drupalgap_page_* and drupalgap_form_*, but adding these prefixes could
+    // get annoying for css selectors used in jQuery and CSS. What to do?
+
+    // If no form id is provided, warn the user.
+    if (!form.id) {
+      return '<p>drupalgap_form_render() - missing form id!</p>' +
+        JSON.stringify(form);
+    }
+    // If the form already exists in the DOM, remove it.
+    if ($('form#' + form.id).length) { $('form#' + form.id).remove(); }
+    // Render the prefix and suffix and wrap them in their own div.
+    var prefix = form.prefix;
+    if (!empty(prefix)) {
+      prefix = '<div class="form_prefix">' + prefix + '</div>';
+    }
+    var suffix = form.suffix;
+    if (!empty(suffix)) {
+      suffix = '<div class="form_suffix">' + suffix + '</div>';
+    }
+    // Render the form's input elements.
+    var form_elements = _drupalgap_form_render_elements(form);
+    var form_attributes = drupalgap_attributes(form.options.attributes);
+    // Return the form html.
+    var form_html = '<div ng-controller="drupalgapFormController"><form ' + form_attributes + '>' +
+      prefix +
+      '<div id="drupalgap_form_errors"></div>' +
+      form_elements +
+      suffix +
+    '</form></div>';
+    return form_html;
+  }
+  catch (error) { console.log('drupalgap_form_render - ' + error); }
+}
+
+/**
+ * Given a form element name and an error message, this attaches the error
+ * message to the drupalgap.form_errors array, keyed by the form element name.
+ * @param {String} name
+ * @param {String} message
+ */
+function drupalgap_form_set_error(name, message) {
+  try {
+    drupalgap.form_errors[name] = message;
+  }
+  catch (error) { console.log('drupalgap_form_set_error - ' + error); }
+}
+
+/**
  * Given a form id, this will delete the form from local storage.
  * If the form isn't in local storage, this returns false.
  * @param {String} form_id
@@ -431,5 +345,84 @@ function drupalgap_form_local_storage_save(form) {
  */
 function drupalgap_form_id_local_storage_key(form_id) {
     return 'drupalgap_form_' + form_id;
+}
+
+/**
+ * Internal function used to dynamically add another element item to a form for
+ * unlimited value fields.
+ * @param {String} form_id
+ * @param {String} name
+ * @param {Number} delta
+ */
+function _drupalgap_form_add_another_item(form_id, name, delta) {
+  try {
+    // Locate the last item, load the form, extract the element from
+    // the form, generate default variables for the new item, determine the next
+    // delta value.
+    var selector = '.' + drupalgap_form_get_element_container_class(name) +
+      ' .drupalgap_form_add_another_item';
+    var add_another_item_button = $(selector);
+    var form = drupalgap_form_local_storage_load(form_id);
+    var language = language_default();
+    var item = drupalgap_form_element_item_create(
+      name,
+      form,
+      language,
+      delta + 1
+    );
+    form.elements[name][language][delta + 1] = item;
+    var element = form.elements[name];
+    var variables = {
+      attributes: {},
+      field_info_field: element.field_info_field,
+      field_info_instance: element.field_info_instance
+    };
+    var field_widget_form_function =
+      element.field_info_instance.widget.module + '_field_widget_form';
+    window[field_widget_form_function].apply(
+      null,
+      _drupalgap_form_element_items_widget_arguments(
+        form,
+        null,
+        element,
+        language,
+        delta + 1
+      )
+    );
+    drupalgap_form_local_storage_save(form);
+    $(add_another_item_button).before(
+      _drupalgap_form_render_element_item(form, element, variables, item)
+    );
+  }
+  catch (error) { console.log('_drupalgap_form_add_another_item - ' + error); }
+}
+
+/**
+ * Returns a 'Cancel' button object that can be used on most forms.
+ * @return {Object}
+ */
+function drupalgap_form_cancel_button() {
+    return {
+      'title': t('Cancel'),
+      attributes: {
+        onclick: 'javascript:drupalgap_back();'
+      }
+    };
+}
+
+/**
+ * Given a jQuery selector to a form, this will clear all the elements on
+ * the UI.
+ * @see http://stackoverflow.com/a/6364313/763010
+ */
+function drupalgap_form_clear(form_selector) {
+  try {
+    $(':input', form_selector)
+     .not(':button, :submit, :reset, :hidden')
+     .val('')
+     .removeAttr('checked')
+     .removeAttr('selected');
+  }
+  catch (error) { console.log('drupalgap_form_clear - ' + error); }
 }
 
