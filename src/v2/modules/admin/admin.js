2bca9bfd1e2dd94713e38cb37349ba644bb13cd5
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-06-29 09:50:45 -0400

    Add admin content page.

--- src/v2/modules/admin/admin.js
+++ src/v2/modules/admin/admin.js
@@ -12,14 +12,45 @@ angular.module('dgAdmin', ['drupalgap'])
           controller: 'dg_page_controller',
           page_callback: 'dg_admin_connect_page'
       });
-}]);
+    $routeProvider.when('/admin/content', {
+      templateUrl: 'themes/spi/page.tpl.html',
+      controller: 'dg_page_controller',
+      page_callback: 'dg_admin_content_page'
+    });
+}])
+  .directive('dgAdminContentPage', function($compile) {
+    return {
+      controller: function($scope, drupal) {
+        $scope.dg_admin = {
+          content: drupal.node_index()
+        };
+      },
+      link: function(scope, element, attrs) {
+        scope.dg_admin.content.then(function(nodes) {
+          var html = '';
+          var items = [];
+          for (var i in nodes) {
+            var node = nodes[i];
+            items.push(l(t(node.title), 'node/' + node.nid));
+          }
+          if (!dg_empty(items)) {
+            html += theme('item_list', { items: items });
+          }
+          var linkFn = $compile(html);
+          var content = linkFn(scope);
+          element.append(content);
+        });
+      }
+    };
+  });
 
 function dg_admin_page() {
   var content = {};
   content['links'] = {
     theme: 'item_list',
     items: [
-      l(t('Connect'), 'admin/connect')
+      l(t('Connect'), 'admin/connect'),
+      l(t('Content'), 'admin/content')
     ]
   };
   return content;
@@ -34,4 +65,19 @@ function dg_admin_connect_page() {
       console.log(result);
   });
 }
+
+function dg_admin_content_page() {
+  try {
+    var content = {};
+    content['nodes'] = {
+      markup: '<dg-admin-content-page></dg-admin-content-page>'
+    };
+    return content;
+  }
+  catch (error) {
+    console.log('dg_admin_content_page - ' + error);
+  }
+}
+
+
 
