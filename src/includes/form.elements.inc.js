7c09d81ea035bea63f120d3afe5d1c5ba6b1290e
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-10-27 19:13:49 -0400

    #654. Support custom form element weights.

--- src/includes/form.elements.inc.js
+++ src/includes/form.elements.inc.js
@@ -121,10 +121,31 @@ function _drupalgap_form_render_elements(form) {
                 form.bundle.indexOf('comment_node_') != -1
               ) { bundle = form.bundle.replace('comment_node_', ''); }
             }
-            // This is not a field, if it has a weight in
-            // field_info_extra_fields use it, otherwise just append it to the
-            // content.
-            if (
+
+            // This is not a field, if it has it's own weight use it, or see if
+            // there is a weight in field_info_extra_fields, otherwise just
+            // append it to the element content.
+
+            // Elements with a weight defined.
+            if (typeof element.weight !== 'undefined') {
+              if (content_weighted[element.weight]) {
+                var msg = 'WARNING: _drupalgap_form_render_elements - the ' +
+                'weight of ' + element.weight + ' for ' + element.name +
+                ' is already in use by ' +
+                content_weighted[element.weight].name;
+                console.log(msg);
+                // Just render it.
+                var _content = _drupalgap_form_render_element(form, element);
+                if (typeof _content !== 'undefined') { content += _content; }
+              }
+              else {
+                content_weighted[element.weight] =
+                  _drupalgap_form_render_element(form, element);
+              }
+            }
+
+            // Extra fields.
+            else if (
               form.entity_type && bundle &&
               typeof drupalgap.field_info_extra_fields[bundle][name] !==
                 'undefined' &&
@@ -134,13 +155,27 @@ function _drupalgap_form_render_elements(form) {
             ) {
               var weight =
                 drupalgap.field_info_extra_fields[bundle][name].weight;
-              content_weighted[weight] =
-              _drupalgap_form_render_element(form, element);
+              if (content_weighted[weight]) {
+                var msg = 'WARNING: _drupalgap_form_render_elements - the ' + 
+                'weight of ' + weight + ' for ' + element.name + ' is ' +
+                'already in use by ' + content_weighted[weight].name;
+                console.log(msg);
+                // Just render it.
+                var _content = _drupalgap_form_render_element(form, element);
+                if (typeof _content !== 'undefined') { content += _content; }
+              }
+              else {
+                content_weighted[weight] =
+                  _drupalgap_form_render_element(form, element);
+              }
             }
+
+            // No weight, just render it.
             else {
               var _content = _drupalgap_form_render_element(form, element);
               if (typeof _content !== 'undefined') { content += _content; }
             }
+
           }
         }
     }
