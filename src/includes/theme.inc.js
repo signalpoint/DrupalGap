2e4c6b8ebc3d5748945b5e596f63013e0d099604
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-12-13 16:19:01 -0500

    Add dg.theme

---
+++ src/includes/theme.inc.js
@@ -0,0 +1,41 @@
+/**
+ * Implementation of theme().
+ * @param {String} hook
+ * @param {Object} variables
+ * @return {String}
+ */
+dg.theme = function(hook, variables) {
+  try {
+
+    // If there is HTML markup present, just return it as is. Otherwise, run
+    // the theme hook and send along the variables.
+    if (!variables) { variables = {}; }
+    if (variables.markup) { return variables.markup; }
+    var content = '';
+
+    // First see if the current theme implements the hook, if it does use it, if
+    // it doesn't fallback to the core theme implementation of the hook.
+    //var theme_function = drupalgap.settings.theme + '_' + hook;
+    //if (!function_exists(theme_function)) {
+      var theme_function = 'theme_' + hook;
+      if (!dg.functionExists(theme_function)) {
+        var caller = null;
+        if (arguments.callee.caller) {
+          caller = arguments.callee.caller.name;
+        }
+        var msg = 'WARNING: ' + theme_function + '() does not exist.';
+        if (caller) { msg += ' Called by: ' + caller + '().' }
+        console.log(msg);
+        return content;
+      }
+    //}
+
+    // Set default attributes.
+    if (!variables.attributes) { variables.attributes = {}; }
+
+    // If there is no class name array, set an empty one.
+    if (!variables.attributes['class']) { variables.attributes['class'] = []; }
+    return window[theme_function].call(null, variables);
+  }
+  catch (error) { console.log('dg.theme - ' + error); }
+};
