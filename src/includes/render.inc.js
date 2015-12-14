bc19cce9045a9575238324ac885b5aaffd1b44e3
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-12-13 20:04:46 -0500

    Simplify dg.render _prefix and _suffix.

--- src/includes/render.inc.js
+++ src/includes/render.inc.js
@@ -6,35 +6,34 @@ dg.render = function(content) {
     var html = '';
     var _html = null;
     if (type === 'object') {
-      if (content.markup) {
-        _html = content.markup;
-        if (content.prefix) { _html = content.prefix + _html; }
-        if (content.suffix) { _html += content.suffix; }
-        return _html;
+      var prefix = content._prefix ? content._prefix : '';
+      var suffix = content._suffix ? content._suffix : '';
+      if (content._markup) {
+        return prefix + content._markup + suffix;
       }
-      if (content.theme) {
-        _html = theme(content.theme, content);
-        if (content.prefix) { _html = content.prefix + _html; }
-        if (content.suffix) { _html += content.suffix; }
-        return _html;
+      if (content._theme) {
+        return prefix + dg.theme(content._theme, content) + suffix;
       }
-      if (content.prefix) { html = content.prefix + html; }
+      if (content._type) {
+        return prefix + dg.theme(content._type, content) + suffix;
+      }
+      html += prefix;
       for (var index in content) {
         if (
           !content.hasOwnProperty(index) ||
-          index == 'prefix' || index == 'suffix'
+          index == '_prefix' || index == '_suffix'
         ) { continue; }
         var piece = content[index];
         var _type = typeof piece;
         console.log('piece: ' + _type);
-        if (_type === 'object') { html += dg_render(piece); }
+        if (_type === 'object') { html += dg.render(piece); }
         else if (_type === 'array') {
           for (var i = 0; i < piece.length; i++) {
             html += dg.render(piece[i]);
           }
         }
       }
-      if (content.suffix) { html += content.suffix; }
+      html += suffix;
     }
     else if (type === 'array') {
       for (var i = 0; i < content.length; i++) {
