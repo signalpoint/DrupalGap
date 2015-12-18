0b5457614ff3fb75c67040bab1805de5b16d53cb
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-12-18 18:03:11 -0500

    Change user login form to use promise.

--- src/modules/user/user.forms.js
+++ src/modules/user/user.forms.js
@@ -2,33 +2,40 @@ var UserLoginForm = function() {
   //this.id = 'UserLoginForm';
 
   this.buildForm = function(form, form_state, options) {
-    this.form._action = dg.config('front'),
-    this.form.name = {
-      _type: 'textfield',
-      _title: 'Username',
-      _required: true,
-      _title_placeholder: true
-    };
-    this.form.pass = {
-      _type: 'password',
-      _title: 'Password',
-      _required: true,
-      _title_placeholder: true
-    };
-    this.form.actions = {
-      _type: 'actions',
-      submit: {
-        _type: 'submit',
-        _value: 'Log in',
-        _button_type: 'primary'
-      }
-    };
-    options.success(form);
+    //var self = this;
+    return new Promise(function(ok, err) {
+      form._action = dg.config('front'),
+      form.name = {
+        _type: 'textfield',
+        _title: 'Username',
+        _required: true,
+        _title_placeholder: true
+      };
+      form.pass = {
+        _type: 'password',
+        _title: 'Password',
+        _required: true,
+        _title_placeholder: true
+      };
+      form.actions = {
+        _type: 'actions',
+        submit: {
+          _type: 'submit',
+          _value: 'Log in',
+          _button_type: 'primary'
+        }
+      };
+      ok(form);
+    });
   };
 
-  this.submitForm = function(options) {
-    var form_state = this.getFormState();
-    jDrupal.userLogin(form_state.values['name'], form_state.values['pass'], options);
+  this.submitForm = function() {
+    var self = this;
+    return new Promise(function(ok, err) {
+      var form_state = self.getFormState();
+      jDrupal.userLogin(form_state.values['name'], form_state.values['pass']).then(ok);
+    });
+
   };
 
 };
