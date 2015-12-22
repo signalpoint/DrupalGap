ee3b2053f54fa341c209152e8a35e2cb1de251e6
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-12-22 09:35:16 -0500

    Wait until all blocks render to process forms.

--- src/includes/render.inc.js
+++ src/includes/render.inc.js
@@ -9,15 +9,17 @@ dg.appRender = function(content) {
     for (var id in regions) {
       if (!regions.hasOwnProperty(id)) { continue; }
 
+      // Instantiate the region and load its blocks.
       var region = new dg.Region({
         id: id,
         attributes: { id: id }
       });
       dg.regions[id] = region;
-
       var blocks = dg.regions[id].getBlocks();
       if (blocks.length == 0) { continue; }
 
+      // Open the region, render the placeholder for each of its block(s), then
+      // close the region.
       innerHTML += '<' + region.get('format')  + ' ' + dg.attributes(region.get('attributes')) + '>';
       for (var i = 0; i < blocks.length; i++) {
         var block = dg.blockLoad(blocks[i]);
@@ -28,35 +30,52 @@ dg.appRender = function(content) {
 
     }
     innerHTML += dg.render(content);
+
+    // Place the region, and block placeholders, into the app's div.
     document.getElementById('dg-app').innerHTML = innerHTML;
 
-    // Run the build promise for each block, then inject their content as the respond.
+    // Run the build promise for each block, then inject their content as they respond.
+    // Keep a tally of all the blocks, and once their promises have all completed, then
+    // if there are any forms on the page, attach their UI submit handlers. We don't use
+    // a promise all, so blocks can render one by one.
     var blocks = dg.blocksLoad();
+    var blocksToRender = [];
     for (id in blocks) {
       if (!blocks.hasOwnProperty(id)) { continue; }
-      var block = blocks[id];
-      block.buildWrapper().then(function(_block) {
+      blocksToRender.push(id);
+      blocks[id].buildWrapper().then(function(_block) {
+
+        // Inject the block content and mark the block as rendered.
         document.getElementById(_block.get('id')).innerHTML = dg.render(_block.get('content'));
+        blocksToRender.splice(blocksToRender.indexOf(_block.get('id')), 1);
+
+        // If we're all done with every block, process the form(s), if any.
+        // @TODO form should be processed as they're injected, because waiting
+        // until all promises have resolved like this means a form can't be used
+        // until they've all resolved.
+        if (blocksToRender.length == 0) {
+          var forms = dg.loadForms();
+          for (var id in forms) {
+            if (!forms.hasOwnProperty(id)) { continue; }
+            var form_html_id = dg.killCamelCase(id, '-');
+            var form = document.getElementById(form_html_id);
+            function processForm(e) {
+              if (e.preventDefault) e.preventDefault();
+              var _form = dg.loadForm(id);
+              _form._submission().then(
+                function() { },
+                function() { }
+              );
+              return false; // Prevent default form behavior.
+            }
+            if (form.attachEvent) { form.attachEvent("submit", processForm); }
+            else { form.addEventListener("submit", processForm); }
+          }
+        }
+
       });
     }
 
-    // Attach UI submit handler for each form on the page, if any.
-    var forms = dg.loadForms();
-    for (var id in forms) {
-      if (!forms.hasOwnProperty(id)) { continue; }
-      var form = document.getElementById(dg.killCamelCase(id, '-'));
-      function processForm(e) {
-        if (e.preventDefault) e.preventDefault();
-        var _form = dg.loadForm(id);
-        _form._submission().then(
-          function() { },
-          function() { }
-        );
-        return false; // Prevent default form behavior.
-      }
-      if (form.attachEvent) { form.attachEvent("submit", processForm); }
-      else { form.addEventListener("submit", processForm); }
-    }
   });
 };
 dg.render = function(content) {
