e087b8d95a6bf5d4dece833a79f7b8f32afbd920
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-12-30 16:34:47 -0500

    Add code comments.

--- src/includes/entity.inc.js
+++ src/includes/entity.inc.js
@@ -1,3 +1,7 @@
-dg.Node = function(nid_or_node) {
-  return new jDrupal.Node(nid_or_node);
-};
+/**
+ * A proxy to create an instance of a jDrupal Node object.
+ * @param nid_or_node
+ * @returns {jDrupal.Node}
+ * @constructor
+ */
+dg.Node = function(nid_or_node) { return new jDrupal.Node(nid_or_node); };
