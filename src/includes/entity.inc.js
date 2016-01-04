36227d6340eba1d9ec9d68c5c6ed0565a8f0b061
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-04 13:55:19 -0500

    Add dg.Node proxy and entityRenderContent.

--- src/includes/entity.inc.js
+++ src/includes/entity.inc.js
@@ -5,3 +5,10 @@
  * @constructor
  */
 dg.Node = function(nid_or_node) { return new jDrupal.Node(nid_or_node); };
+
+dg.entityRenderContent = function(entity) {
+  var content = {};
+  var label = entity.getEntityKey('label');
+  content[label] = { _markup: '<h2>' + entity.label() + '</h2>' };
+  return content;
+};
