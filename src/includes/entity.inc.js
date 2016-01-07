9dc30b40d302913870b1850acdf05ea91d089c00
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-06 19:41:05 -0500

    Change entityRenderContent to a promise.

--- src/includes/entity.inc.js
+++ src/includes/entity.inc.js
@@ -7,44 +7,50 @@
 dg.Node = function(nid_or_node) { return new jDrupal.Node(nid_or_node); };
 
 dg.entityRenderContent = function(entity) {
-  var entityType = entity.getEntityType();
-  var bundle = entity.getBundle();
-  var label = entity.getEntityKey('label');
 
-  // Build the render array for the entity...
-  var content = {};
+  return new Promise(function(ok, err) {
 
-  // Add the entity label.
-  content[label] = {
-    _theme: 'entity_label',
-    _entity: entity,
-    _attributes: {
-      'class': [entityType + '-title']
+    var entityType = entity.getEntityType();
+    var bundle = entity.getBundle();
+    var label = entity.getEntityKey('label');
+
+    // Build the render array for the entity...
+    var content = {};
+
+    // Add the entity label.
+    content[label] = {
+      _theme: 'entity_label',
+      _entity: entity,
+      _attributes: {
+        'class': [entityType + '-title']
+      }
+    };
+
+    //console.log(dg);
+    //console.log(dg.entity_view_mode);
+
+    // Iterate over each field in the drupalgap entity view mode.
+    var viewMode = bundle ? dg.entity_view_mode[entityType][bundle] : dg.entity_view_mode[entityType];
+    for (var fieldName in viewMode) {
+      if (!viewMode.hasOwnProperty(fieldName)) { continue; }
+      //console.log(fieldName);
+      //console.log(viewMode[fieldName]);
+
+      // Grab the field storage config and the module in charge of the field.
+      var fieldStorageConfig = dg.fieldStorageConfig[entityType][fieldName];
+      if (!fieldStorageConfig) { continue; }
+      //console.log(fieldStorageConfig);
+      var module = fieldStorageConfig.module;
+      if (!jDrupal.moduleExists(module)) {
+        var msg = 'WARNING - entityRenderContent - The "' + module + '" module is not present to render the "' + fieldName + '" field.';
+        console.log(msg);
+        continue;
+      }
     }
-  };
+    jDrupal.moduleInvokeAll('entity_view', content, entity).then(ok(content));
 
-  //console.log(dg);
-  //console.log(dg.entity_view_mode);
+  });
 
-  // Iterate over each field in the drupalgap entity view mode.
-  var viewMode = bundle ? dg.entity_view_mode[entityType][bundle] : dg.entity_view_mode[entityType];
-  for (var fieldName in viewMode) {
-    if (!viewMode.hasOwnProperty(fieldName)) { continue; }
-    console.log(fieldName);
-    console.log(viewMode[fieldName]);
-
-    // Grab the field storage config and the module in charge of the field.
-    var fieldStorageConfig = dg.fieldStorageConfig[entityType][fieldName];
-    if (!fieldStorageConfig) { continue; }
-    console.log(fieldStorageConfig);
-    var module = fieldStorageConfig.module;
-    if (!jDrupal.moduleExists(module)) {
-      var msg = 'WARNING - entityRenderContent - The "' + module + '" module is not present to render the "' + fieldName + '" field.';
-      console.log(msg);
-      continue;
-    }
-  }
-  return content;
 };
 
 dg.theme_entity_label = function(variables) {
