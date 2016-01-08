2281f3347f3eeb4029f524886fd518f45304a115
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-07 19:42:02 -0500

    Update binaries.

--- drupalgap.js
+++ drupalgap.js
@@ -364,6 +364,7 @@ dg.FieldDefinitionInterface = function(entityType, bundle, fieldName) {
   this.bundle = bundle;
   this.fieldName = fieldName;
   this.fieldDefinition = dg.fieldDefinitions[entityType][bundle][fieldName];
+  console.log(this.fieldDefinition);
 };
 dg.FieldDefinitionInterface.prototype.get = function(prop) {
   return typeof this.fieldDefinition[prop] ? this.fieldDefinition[prop] : null;
@@ -1498,6 +1499,28 @@ dg.modules.system.blocks = function() {
   };
   return blocks;
 };
+dg.modules.text = new dg.Module();
+
+// Let DrupalGap know we have a FieldWidget(s).
+dg.modules.text.FieldWidget = {};
+
+// Text_with_summary field.
+dg.modules.text.FieldWidget.text_with_summary = function(entityType, bundle, fieldName, fieldFormMode) {
+
+  dg.FieldWidgetPrepare(this, arguments);
+
+  this.form = function(items, form, formState) {
+    return {
+      _type: 'textarea',
+      _title: this.fieldDefinition.getLabel(),
+      _title_placeholder: true
+    };
+  };
+
+};
+// Extend the FieldWidget prototype for the Text_with_summary field.
+dg.modules.text.FieldWidget.text_with_summary.prototype = new dg.FieldWidget;
+dg.modules.text.FieldWidget.text_with_summary.prototype.constructor = dg.modules.text.FieldWidget.text_with_summary;
 var UserLoginForm = function() {
 
   this.buildForm = function(form, formState) {
