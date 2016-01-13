f348a5a5abea61064455bbd8edf1f30b91ec8829
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-13 01:10:05 -0500

    Make FormWidget extend FormElement.

--- src/includes/form-widget.inc.js
+++ src/includes/form-widget.inc.js
@@ -1,6 +1,10 @@
 dg.FormWidget = function(entityType, bundle, fieldName) {
   // Any default constructor behavior lives in FormWidgetPrepare
 };
+// Extend the FormElement prototype.
+dg.FormWidget.prototype = new dg.FormElement;
+dg.FormWidget.prototype.constructor = dg.FormWidget;
+
 /**
  * Used to prepare a Form Widget default constructor.
  * @param FormWidget
@@ -8,8 +12,24 @@ dg.FormWidget = function(entityType, bundle, fieldName) {
  * @constructor
  */
 dg.FormWidgetPrepare = function(FormWidget, args) {
+  FormWidget.widgetType = 'FormWidget';
   FormWidget.entityType = args[0];
   FormWidget.bundle = args[1];
   FormWidget.fieldName = args[2];
-  FormWidget.fieldFormMode = args[3];
+  FormWidget.name = FormWidget.fieldName;
+  FormWidget.element = args[3];
+  FormWidget.fieldFormMode = FormWidget.element._fieldFormMode;
+};
+
+dg.FormWidget.prototype.valueCallback = function() {
+  var self = this;
+  return new Promise(function(ok, err) {
+    var value = null;
+    var el = document.getElementById(self.id());
+    if (el) { value = el.value; }
+    ok({
+      name: self.get('name'),
+      value: [ { value: value }  ]
+    });
+  });
 };
