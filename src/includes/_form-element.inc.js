0a41a2dc12bd53288a7b3534a2bdcdf712ca0b79
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-13 01:08:05 -0500

    Fallback to a null id on FormElement.

--- src/includes/_form-element.inc.js
+++ src/includes/_form-element.inc.js
@@ -1,3 +1,6 @@
+// We prefixed this file name with an underscore so that dg.FormElement is available quickly. This is our cheap fix
+// until we figure out a better Gruntfile to handle this.
+
 // @see https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Render!Element!FormElementInterface.php/interface/FormElementInterface/8
 
 /**
@@ -12,7 +15,7 @@ dg.FormElement = function(name, element, form) {
   this.element = element; // Holds the form element JSON object provided by the form builder.
   this.form = form;
 };
-dg.FormElement.prototype.id = function() { return this.element._attributes.id; };
+dg.FormElement.prototype.id = function() { return this.element ? this.element._attributes.id : null; };
 dg.FormElement.prototype.getForm = function() { return this.form; };
 dg.FormElement.prototype.get = function(property) {
   return typeof this[property] ? this[property] : null;
