00941c0ec475145798a6e5468dce2191cfc8311f
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-13 01:08:54 -0500

    Make FieldWidget extend FormElement.

--- src/includes/field-widget.inc.js
+++ src/includes/field-widget.inc.js
@@ -1,8 +1,11 @@
 // @see https://api.drupal.org/api/drupal/core!modules!field!field.api.php/group/field_widget/8
 // @see http://capgemini.github.io/drupal/writing-custom-fields-in-drupal-8/
-dg.FieldWidget = function(entityType, bundle, fieldName) {
+dg.FieldWidget = function(entityType, bundle, fieldName, element) {
   // Any default constructor behavior lives in FieldWidgetPrepare
 };
+// Extend the FormElement prototype.
+dg.FieldWidget.prototype = new dg.FormElement;
+dg.FieldWidget.prototype.constructor = dg.FieldWidget;
 /**
  * Used to prepare a Field Widget default constructor.
  * @param FieldWidget
@@ -10,15 +13,18 @@ dg.FieldWidget = function(entityType, bundle, fieldName) {
  * @constructor
  */
 dg.FieldWidgetPrepare = function(FieldWidget, args) {
+  FieldWidget.widgetType = 'FieldWidget';
   FieldWidget.entityType = args[0];
   FieldWidget.bundle = args[1];
   FieldWidget.fieldName = args[2];
+  FieldWidget.name = FieldWidget.fieldName;
+  FieldWidget.element = args[3];
   FieldWidget.fieldDefinition = new dg.FieldDefinitionInterface(
       FieldWidget.entityType,
       FieldWidget.bundle,
       FieldWidget.fieldName
   );
-  FieldWidget.fieldFormMode = args[3];
+  FieldWidget.fieldFormMode = FieldWidget.element._fieldFormMode;
 };
 dg.FieldWidget.prototype.getSetting = function(prop) {
   return typeof this.settings[prop] ? this.settings[prop] : null;
@@ -31,4 +37,17 @@ dg.FieldWidget.prototype.getSettings = function() {
 };
 dg.FieldWidget.prototype.setSettings = function(val) {
   this.settings = val;
+};
+
+dg.FieldWidget.prototype.valueCallback = function() {
+  var self = this;
+  return new Promise(function(ok, err) {
+    var value = null;
+    var el = document.getElementById(self.id());
+    if (el) { value = el.value; }
+    ok({
+      name: self.get('name'),
+      value: [ { value: value }  ]
+    });
+  });
 };
