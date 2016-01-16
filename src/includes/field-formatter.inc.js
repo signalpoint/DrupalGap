e7945d3ade37d306c40fd7e5cf3168aef84097c5
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-16 17:48:14 -0500

    Set up FieldFormatterPrepare and viewElements skeleton.

--- src/includes/field-formatter.inc.js
+++ src/includes/field-formatter.inc.js
@@ -1,7 +1,13 @@
 // @see https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Field!FormatterBase.php/class/FormatterBase/8
 // @see https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Field!Annotation!FieldFormatter.php/class/FieldFormatter/8
 
-dg.FieldFormatter = function() { };
+dg.FieldFormatter = function() {
+  this._fieldDefinition = null;
+  this._settings = null;
+  this._label = null;
+  this._viewMode = null;
+  this._thirdPartySettings = null;
+};
 
 /**
  * Used to prepare a field formatter default constructor.
@@ -9,15 +15,21 @@ dg.FieldFormatter = function() { };
  * @param args
  * @constructor
  */
-dg.FieldFormatterPrepare = function(FieldFormatter, args) { };
+dg.FieldFormatterPrepare = function(FieldFormatter, args) {
+  this._fieldDefinition = args[0];
+  this._settings = args[1];
+  this._label = args[2];
+  this._viewMode = args[3];
+  this._thirdPartySettings = args[4];
+};
 
 // Builds a renderable array for a field value.
-dg.FieldFormatter.prototype.viewElements = function(items, langcode) {
+dg.FieldFormatter.prototype.viewElements = function(FieldItemListInterface, langcode) {
+  var items = FieldItemListInterface.getItems();
   var element = {};
   if (items.length == 0) { return element; }
   for (var delta = 0; delta < items.length; delta++) {
-    element[delta] = items[delta];
+    element[delta] = { _markup: items[delta].value };
   }
   return element;
 };
-
