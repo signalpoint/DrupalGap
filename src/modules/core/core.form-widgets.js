339ea65fde981e721eaf4ee3c0fd2663c711ae82
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-17 17:30:27 -0500

    Remove some code comments.

--- src/modules/core/core.form-widgets.js
+++ src/modules/core/core.form-widgets.js
@@ -1,14 +1,14 @@
 // Let DrupalGap know we have a FormWidget(s).
 dg.modules.core.FormWidget = {};
 
+// @TODO the "items" args here should be prototypes of field-item-list-interface
+
 // String textfield widget.
-// Extend the FormWidget prototype for the string_textfield widget.
 dg.modules.core.FormWidget.string_textfield = function(entityType, bundle, fieldName, element, items, delta) {
   dg.FormWidgetPrepare(this, arguments);
 };
 dg.modules.core.FormWidget.string_textfield.prototype = new dg.FormWidget;
 dg.modules.core.FormWidget.string_textfield.prototype.constructor = dg.modules.core.FormWidget.string_textfield;
-
 dg.modules.core.FormWidget.string_textfield.prototype.form = function(items, delta, element, form, formState) {
   element._type = 'textfield';
   element._title = this.fieldName;
@@ -24,13 +24,11 @@ dg.modules.core.FormWidget.string_textfield.prototype.form = function(items, del
 // @TODO Move the bundle and entityID widgets to the entity module.
 
 // Bundle widget.
-// Extend the FormWidget prototype for the bundle widget.
 dg.modules.core.FormWidget.bundle = function(entityType, bundle, fieldName, element, items, delta) {
   dg.FormWidgetPrepare(this, arguments);
 };
 dg.modules.core.FormWidget.bundle.prototype = new dg.FormWidget;
 dg.modules.core.FormWidget.bundle.prototype.constructor = dg.modules.core.FormWidget.bundle;
-
 dg.modules.core.FormWidget.bundle.prototype.form = function(items, delta, element, form, formState) {
   element._type = 'hidden';
   if (items && items[delta] !== 'undefined') {
@@ -47,13 +45,11 @@ dg.modules.core.FormWidget.bundle.prototype.valueCallback = function(items, form
 };
 
 // entityID widget.
-// Extend the FormWidget prototype for the entityID widget.
 dg.modules.core.FormWidget.entityID = function(entityType, bundle, fieldName, element, items, delta) {
   dg.FormWidgetPrepare(this, arguments);
 };
 dg.modules.core.FormWidget.entityID.prototype = new dg.FormWidget;
 dg.modules.core.FormWidget.entityID.prototype.constructor = dg.modules.core.FormWidget.entityID;
-
 dg.modules.core.FormWidget.entityID.prototype.form = function(items, delta, element, form, formState) {
   element._type = 'hidden';
   if (items && items[delta] !== 'undefined') {
