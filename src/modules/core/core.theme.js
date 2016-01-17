2e6eadbce50f2d431433220bf6cfa0c68d4323a1
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-17 13:21:37 -0500

    Add theme_container.

--- src/modules/core/core.theme.js
+++ src/modules/core/core.theme.js
@@ -1,7 +1,10 @@
+dg.theme_container = function(variables) {
+  return '<div ' + dg.attributes(variables._attributes) + '>' +
+      dg.render(variables._children) +
+      '</div>';
+};
 dg.theme_entity_reference_label = function(variables) {
   var item = variables._item;
-  console.log(item);
-  console.log(dg.path());
   return dg.l(item.target_id, item.url.replace(dg.path(), ''));
 };
 dg.theme_string = function(variables) {
