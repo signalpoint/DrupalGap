5215ce9b46a315dc04c3541af66d3a306b696c7b
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-19 18:29:50 -0500

    Add dashboard and 404 pages.

--- src/modules/system/system.js
+++ src/modules/system/system.js
@@ -1,5 +1,39 @@
 dg.modules.system = new dg.Module();
 
+dg.modules.system.routing = function() {
+  var routes = {};
+  routes["system.dashboard"] = {
+    "path": "/dg",
+    "defaults": {
+      "_title": "DrupalGap Dashboard",
+      _controller: function() {
+        return new Promise(function(ok, err) {
+          var msg = 'Welcome to DrupalGap!';
+          var account = dg.currentUser();
+          if (account.isAuthenticated()) {
+            msg = msg.replace('!', ', ' + account.getAccountName() + '!');
+          }
+          ok('<p>' + msg + '</p>');
+        });
+
+      }
+    }
+  };
+  routes["system.404"] = {
+    "path": "/404",
+    "defaults": {
+      "_title": "404 - Page not found",
+      _controller: function() {
+        return new Promise(function(ok, err) {
+          ok('Sorry, that page was not found...');
+        });
+
+      }
+    }
+  };
+  return routes;
+};
+
 dg.modules.system.blocks = function() {
   var blocks = {};
   blocks.main = {
