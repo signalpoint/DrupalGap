7ec81f563a6d6ece8da33b3ca29ea741d5157f35
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-19 22:36:01 -0500

    Improve theme_link for front page. Add first and last class to items in theme_item_list.

--- src/includes/widgets.inc.js
+++ src/includes/widgets.inc.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * @param text
+ * @param path
+ * @param options
+ * @returns {String}
+ */
 dg.l = function(text, path, options) {
   if (!options) { options = {}; }
   if (!options._text) { options._text = text; }
@@ -11,9 +18,9 @@ dg.l = function(text, path, options) {
  */
 dg.theme_link = function(variables) {
   var text = variables._text ? variables._text : '';
-  if (typeof variables._attributes.href === 'undefined' && variables._path) {
-    variables._attributes.href = '#' + variables._path;
-  }
+  var path = variables._path;
+  if (path == '') { path = dg.getFrontPagePath(); }
+  if (typeof variables._attributes.href === 'undefined' && path) { variables._attributes.href = '#' + path; }
   return '<a ' + dg.attributes(variables._attributes) + '>' + text + '</a>';
 };
 
@@ -31,7 +38,10 @@ dg.theme_item_list = function(variables) {
     for (var i in variables._items) {
       if (!variables._items.hasOwnProperty(i)) { continue; }
       var item = variables._items[i];
-      html += '<li>' + item + '</li>';
+      var attrs = {};
+      if (i == 0) { attrs['class'] = ['first']; }
+      else if (i == variables._items.length - 1) { attrs['class'] = ['last']; }
+      html += '<li ' + dg.attributes(attrs) + '>' + item + '</li>';
     }
   }
   return html += '</' + type + '>';
