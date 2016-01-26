2f3d3e71b3ff0ac879a894ed98f905bf822e7ec2
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-25 19:57:47 -0500

    Remove icon from admin menu.

--- src/modules/admin/admin.js
+++ src/modules/admin/admin.js
@@ -5,14 +5,15 @@ dg.modules.admin.blocks = function() {
   blocks.admin_menu = {
     build: function () {
       return new Promise(function(ok, err) {
+        //dg.theme('image', { _path: 'favicon.ico' }
         var content = {};
         content['menu'] = {
           _theme: 'item_list',
           _items: [
-            dg.l(dg.theme('image', { _path: 'favicon.ico' }), ''),
-            dg.l('Content', 'node/add'),
-            dg.l('My account', 'user/' + dg.currentUser().id()),
-            dg.l('Logout', 'user/logout')
+            dg.l(dg.t('Home'), ''),
+            dg.l(dg.t('Content'), 'node/add'),
+            dg.l(dg.t('My account'), 'user/' + dg.currentUser().id()),
+            dg.l(dg.t('Logout'), 'user/logout')
           ]
         };
         ok(content);
