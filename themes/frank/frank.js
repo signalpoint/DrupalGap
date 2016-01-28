f50ce39eadd79308fc73cb19860814b93287230a
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-01-27 20:23:33 -0500

    Move frank into dg.themes namespace and improve foundation classes for regions.

--- themes/frank/frank.js
+++ themes/frank/frank.js
@@ -1,22 +1,22 @@
-Frank = function() {
+dg.themes.Frank = function() {
   this.regions = {
     header: {
-      attributes: {
-        'class': 'row'
-      }
+      _format: 'header',
+      _prefix: '<div class="top-bar">',
+      _suffix: '</div>'
     },
     content: {
-      attributes: {
-        'class': 'row'
+      _attributes: {
+        'class': 'row columns'
       }
     },
     footer: {
-      attributes: {
-        'class': 'row'
-      }
+      _format: 'footer',
+      _prefix: '<div class="row">',
+      _suffix: '</div>'
     }
   };
 };
 // Extend the DrupalGap Theme prototype.
-Frank.prototype = new dg.Theme;
-Frank.prototype.constructor = Frank;
+dg.themes.Frank.prototype = new dg.Theme;
+dg.themes.Frank.prototype.constructor = dg.themes.Frank;
