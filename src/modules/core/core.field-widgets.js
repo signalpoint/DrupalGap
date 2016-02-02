a41e3ca7b97060535e497629cf03ebd852260789
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-02-02 00:06:58 -0500

    Add float field support.

--- src/modules/core/core.field-widgets.js
+++ src/modules/core/core.field-widgets.js
@@ -1,6 +1,27 @@
 // Let DrupalGap know we have a FieldWidget(s).
 dg.modules.core.FieldWidget = {};
 
+// Float field.
+// Extend the FieldWidget prototype for the Float field.
+dg.modules.core.FieldWidget.float = function(entityType, bundle, fieldName, element, items, delta) {
+  dg.FieldWidgetPrepare(this, arguments);
+};
+dg.modules.core.FieldWidget.float.prototype = new dg.FieldWidget;
+dg.modules.core.FieldWidget.float.prototype.constructor = dg.modules.core.FieldWidget.float;
+
+dg.modules.core.FieldWidget.float.prototype.form = function(items, delta, element, form, formState) {
+  element._type = 'number';
+  element._title = this.fieldDefinition.getLabel();
+  element._title_placeholder = true;
+  element._widgetType = 'FieldWidget';
+  element._module = 'core';
+  element._attributes.step = 'any';
+  if (items && items[delta] !== 'undefined') {
+    element._value = items[delta].value;
+    element._attributes.value = element._value;
+  }
+};
+
 // Integer field.
 // Extend the FieldWidget prototype for the Integer field.
 dg.modules.core.FieldWidget.integer = function(entityType, bundle, fieldName, element, items, delta) {
