3f449a420bb0f4f3c966b72e9279db81a0262a93
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-02-03 23:45:22 -0500

    Add number_decimal and number_float FieldFormatter.

--- src/modules/core/core.field-formatters.js
+++ src/modules/core/core.field-formatters.js
@@ -3,7 +3,7 @@ dg.modules.core = new dg.Module();
 // Let DrupalGap know we have a FieldFormatter(s).
 dg.modules.core.FieldFormatter = {};
 
-// Number integer field formatter.
+// Entity reference field formatter.
 // Extend the FieldFormatter prototype for the number_integer field.
 dg.modules.core.FieldFormatter.entity_reference_label = function() { dg.FieldFormatterPrepare(this, arguments); };
 dg.modules.core.FieldFormatter.entity_reference_label.prototype = new dg.FieldFormatter;
@@ -15,6 +15,42 @@ dg.modules.core.FieldFormatter.entity_reference_label.prototype.viewElements = f
   for (var delta = 0; delta < items.length; delta++) {
     element[delta] = {
       _theme: 'entity_reference_label',
+      _item: items[delta]
+    };
+  }
+  return element;
+};
+
+// Number decimal field formatter.
+// Extend the FieldFormatter prototype for the number_decimal field.
+dg.modules.core.FieldFormatter.number_decimal = function() { dg.FieldFormatterPrepare(this, arguments); };
+dg.modules.core.FieldFormatter.number_decimal.prototype = new dg.FieldFormatter;
+dg.modules.core.FieldFormatter.number_decimal.prototype.constructor = dg.modules.core.FieldFormatter.number_decimal;
+dg.modules.core.FieldFormatter.number_decimal.prototype.viewElements = function(FieldItemListInterface, langcode) {
+  var items = FieldItemListInterface.getItems();
+  var element = {};
+  if (items.length == 0) { return element; }
+  for (var delta = 0; delta < items.length; delta++) {
+    element[delta] = {
+      _theme: 'number_decimal',
+      _item: items[delta]
+    };
+  }
+  return element;
+};
+
+// Number float field formatter.
+// Extend the FieldFormatter prototype for the number_float field.
+dg.modules.core.FieldFormatter.number_float = function() { dg.FieldFormatterPrepare(this, arguments); };
+dg.modules.core.FieldFormatter.number_float.prototype = new dg.FieldFormatter;
+dg.modules.core.FieldFormatter.number_float.prototype.constructor = dg.modules.core.FieldFormatter.number_float;
+dg.modules.core.FieldFormatter.number_float.prototype.viewElements = function(FieldItemListInterface, langcode) {
+  var items = FieldItemListInterface.getItems();
+  var element = {};
+  if (items.length == 0) { return element; }
+  for (var delta = 0; delta < items.length; delta++) {
+    element[delta] = {
+      _theme: 'number_float',
       _item: items[delta]
     };
   }
