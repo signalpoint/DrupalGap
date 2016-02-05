9de3ec1e1d3e700c86df0c723ecfa92b840bd961
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-02-04 21:04:17 -0500

    Unwrap user fieldDefinition from bundle.

--- src/includes/field-definition.inc.js
+++ src/includes/field-definition.inc.js
@@ -2,7 +2,9 @@ dg.FieldDefinitionInterface = function(entityType, bundle, fieldName) {
   this.entityType = entityType;
   this.bundle = bundle;
   this.fieldName = fieldName;
-  this.fieldDefinition = dg.fieldDefinitions[entityType][bundle][fieldName];
+  // Unwrap user entities from the bundle, leave all others as is.
+  this.fieldDefinition = entityType == 'user' ?
+      dg.fieldDefinitions[entityType][fieldName] : dg.fieldDefinitions[entityType][bundle][fieldName];
 };
 dg.FieldDefinitionInterface.prototype.get = function(prop) {
   return typeof this.fieldDefinition[prop] ? this.fieldDefinition[prop] : null;
