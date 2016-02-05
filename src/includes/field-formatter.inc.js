4a2b4311b4b278019e0f8aa6b08d42e349752bc4
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-02-04 21:04:50 -0500

    Properly set FieldFormatter vals in prep function.

--- src/includes/field-formatter.inc.js
+++ src/includes/field-formatter.inc.js
@@ -1,29 +1,39 @@
-// @see https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Field!FormatterBase.php/class/FormatterBase/8
-// @see https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Field!Annotation!FieldFormatter.php/class/FieldFormatter/8
-
+/**
+ * The FieldFormatter constructor stub.
+ * @constructor
+ */
 dg.FieldFormatter = function() {
-  this._fieldDefinition = null;
-  this._settings = null;
-  this._label = null;
-  this._viewMode = null;
-  this._thirdPartySettings = null;
+
+  // This is a constructor stub, all FieldFormatter implementations should call dg.FieldFormatterPrepare in their
+  // constructors.
+
+  //this._fieldDefinition = null;
+  //this._settings = null;
+  //this._label = null;
+  //this._viewMode = null;
+  //this._thirdPartySettings = null;
 };
 
 /**
  * Used to prepare a field formatter default constructor.
- * @param FieldFormatter
- * @param args
+ * @param {Object} FieldFormatter
+ * @param {Array} args
  * @constructor
  */
 dg.FieldFormatterPrepare = function(FieldFormatter, args) {
-  this._fieldDefinition = args[0];
-  this._settings = args[1];
-  this._label = args[2];
-  this._viewMode = args[3];
-  this._thirdPartySettings = args[4];
+  FieldFormatter._fieldDefinition = args[0];
+  FieldFormatter._settings = args[1];
+  FieldFormatter._label = args[2];
+  FieldFormatter._viewMode = args[3];
+  FieldFormatter._thirdPartySettings = args[4];
 };
 
-// Builds a renderable array for a field value.
+/**
+ * Builds a renderable array for a field value.
+ * @param {Object} FieldItemListInterface
+ * @param {String} langcode
+ * @returns {Object}
+ */
 dg.FieldFormatter.prototype.viewElements = function(FieldItemListInterface, langcode) {
   var items = FieldItemListInterface.getItems();
   var element = {};
