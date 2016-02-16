e3f410ff00989b50c22a866b78d86d7739ac55ad
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-02-16 11:16:03 -0500

    Add some spaces for better code reading.

--- src/modules/field/field.js
+++ src/modules/field/field.js
@@ -140,6 +140,7 @@ function drupalgap_field_info_instances_add_to_form(entity_type, bundle,
           }
           if (entity && entity[name] && entity[name].length != 0 && entity[name][language]) {
             for (var delta = 0; delta < cardinality; delta++) {
+
               // @TODO - is this where we need to use the idea of the
               // value_callback property present in Drupal's FAPI? That way
               // each element knows how to map the entity data to its element
@@ -148,18 +149,22 @@ function drupalgap_field_info_instances_add_to_form(entity_type, bundle,
                 entity[name][language][delta] &&
                 typeof entity[name][language][delta].value !== 'undefined'
               ) { default_value = entity[name][language][delta].value; }
+
               // If the default_value is null, set it to an empty string.
               if (default_value == null) { default_value = ''; }
+
               // @todo - It appears not all fields have a language code to use
               // here, for example taxonomy term reference fields don't!
               form.elements[name][language][delta] = {
                 value: default_value
               };
+
               // Place the field item onto the element.
               if (entity[name][language][delta]) {
                 form.elements[name][language][delta].item =
                   entity[name][language][delta];
               }
+
             }
           }
 
