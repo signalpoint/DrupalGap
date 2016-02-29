75ba16e6d26d955d15401b847d4d9173607e7ed9
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-02-29 17:43:13 -0500

    Update binaries.

--- bin/drupalgap.js
+++ bin/drupalgap.js
@@ -1,4 +1,4 @@
-/*! drupalgap 2016-02-28 */
+/*! drupalgap 2016-02-29 */
 // Initialize the drupalgap json object.
 var drupalgap = drupalgap || drupalgap_init(); // Do not remove this line.
 
@@ -1840,28 +1840,11 @@ function _theme_autocomplete(list, e, data, autocomplete_id) {
           };
           options.parameters[autocomplete.filter] = '%' + value + '%';
           options.parameters_op[autocomplete.filter] = 'like';
-          var bundles = [];
-          for (var bundle in field_settings.handler_settings.target_bundles) {
-              if (!field_settings.handler_settings.target_bundles.hasOwnProperty(bundle)) { continue; }
-              var name = field_settings.handler_settings.target_bundles[bundle];
-              var bundle_name = null;
-              switch (field_settings.target_type) {
-                case 'node':
-                  bundle_name = 'type';
-                  break;
-                case 'taxonomy_term':
-                  bundle_name = 'vid';
-                  bundle = taxonomy_vocabulary_get_vid_from_name(bundle);
-                  break;
-              }
-              if (bundle_name) { bundles.push(bundle); }
-          }
-          if (bundles.length) { options.parameters[bundle_name] = bundles.join(','); }
-          var fn = window[index_resource];
-          fn(options, {
+          var bundles = entityreference_get_target_bundles(field_settings);
+          if (bundles) { options.parameters[entity_get_bundle_name(field_settings.target_type)] = bundles.join(','); }
+          window[index_resource](options, {
               success: function(results) {
-                var fn = _theme_autocomplete_success_handlers[autocomplete_id];
-                fn(autocomplete_id, results, false);
+                _theme_autocomplete_success_handlers[autocomplete_id](autocomplete_id, results, false);
               }
           });
           break;
@@ -4135,10 +4118,12 @@ function theme_select(variables) {
           if (value == 'attributes') { continue; } // Skip the attributes.
           // Is the option selected?
           var selected = '';
-          if (
-            typeof variables.value !== 'undefined' &&
-            variables.value == value
-          ) { selected = ' selected '; }
+          if (typeof variables.value !== 'undefined') {
+            if (
+              ($.isArray(variables.value) && in_array(value, variables.value)) ||
+              variables.value == value
+            ) { selected = ' selected '; }
+          }
           // Render the option.
           options += '<option value="' + value + '" ' + selected + '>' +
             label +
