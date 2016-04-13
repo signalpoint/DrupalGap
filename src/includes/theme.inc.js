e4ad2447ac7f6b77d125032a033b0d748e0588d5
JF <jfcartier@kiwad.com>
2016-04-13 18:31:03 -0400

    Undefined test added on _callback feature

--- src/includes/theme.inc.js
+++ src/includes/theme.inc.js
@@ -74,7 +74,7 @@ dg.theme = function(hook, variables) {
     if (html instanceof Promise) {
       html.then(function(data) {
         document.getElementById(data.variables._attributes.id).innerHTML = dg.render(data.content);
-        if (data.variables._callback.length) {
+        if (typeof data.variables._callback !== 'undefined' && data.variables._callback.length) {
           for (var i = 0; i < data.variables._callback.length; i++) { data.variables._callback[i](data); }
           data.variables._callback = [];
         }
