4c43296a79104169f384256695bbf2d41f317537
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-10-13 23:48:49 -0400

    #867 Update ava and easystreet3 themes to use hook_TYPE_tpl_html().

--- themes/ava/ava.js
+++ themes/ava/ava.js
@@ -32,3 +32,33 @@ function ava_info() {
   catch (error) { drupalgap_error(error); }
 }
 
+/**
+ * Implements hook_TYPE_tpl_html().
+ */
+function ava_page_tpl_html() {
+  return '<div {:drupalgap_page_attributes:}>' +
+      '{:header:}' +
+      '{:content:}' +
+      '{:footer:}' +
+      '</div>';
+}
+
+/**
+ * Implements hook_TYPE_tpl_html().
+ */
+function ava_node_tpl_html() {
+  return '<h2>{:title:}</h2>' +
+      '<div>{:content:}</div>' +
+      '<div>{:comments:}</div>' +
+      '<div>{:comments_form:}</div>';
+}
+
+/**
+ * Implements hook_TYPE_tpl_html().
+ */
+function ava_user_profile_tpl_html() {
+  return '<h2>{:name:}</h2>' +
+      '<div>{:created:}</div>' +
+      '<div class="user-picture">{:picture:}</div>' +
+      '<div>{:content:}</div>';
+}
