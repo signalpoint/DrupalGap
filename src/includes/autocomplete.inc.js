8c1c640cc9b5b6dc5c4699fe6be975a02e7e78e3
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2016-11-11 16:02:44 -0500

    Support the 'parent' filter with taxonomy term index on autocompletes.

--- src/includes/autocomplete.inc.js
+++ src/includes/autocomplete.inc.js
@@ -345,9 +345,8 @@ function _theme_autocomplete(list, e, data, autocomplete_id) {
               entity_primary_key_title(autocomplete.entity_type)
             ];
             if (autocomplete.entity_type == 'taxonomy_term') {
-              if (autocomplete.vid) {
-                query.parameters['vid'] = autocomplete.vid;
-              }
+              if (autocomplete.vid) { query.parameters['vid'] = autocomplete.vid; }
+              if (autocomplete.parent) { query.parameters['parent'] = autocomplete.parent; }
             }
             query.fields = fields;
             query.parameters[autocomplete.filter] = '%' + value + '%';
