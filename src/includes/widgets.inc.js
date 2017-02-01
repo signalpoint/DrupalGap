016be7f29823b6c411ee05bd7fe1d266b58a50ba
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2017-02-01 11:00:07 -0500

    Add _prefix and _suffix to theme_bucket().

--- src/includes/widgets.inc.js
+++ src/includes/widgets.inc.js
@@ -4,8 +4,12 @@ dg.theme_bucket = function(variables) {
   if (!variables._grab) { return; }
   var format = variables._format;
   var element = {};
+  var prefix = variables._prefix ? dg.render(variables._prefix) : '';
+  var suffix = variables._suffix ? dg.render(variables._suffix) : '';
   element.bucket = {
-    _markup: '<' + format + ' ' + dg.attributes(variables._attributes) + '></' + format + '>',
+    _markup: prefix +
+        '<' + format + ' ' + dg.attributes(variables._attributes) + '></' + format + '>' +
+      suffix,
     _postRender: [function() {
       variables._grab().then(function(content) {
         document.getElementById(variables._attributes.id).innerHTML = dg.render(content);
