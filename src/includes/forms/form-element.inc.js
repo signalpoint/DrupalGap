2125397259b0fec735cd2db7e5a451d0f55b77c6
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-03-19 00:45:17 -0400

    Add getDomElement() and validateValue() to the dg.FormElement prototype. Add a dg._formElementPrepare() wrapper to hopefully share with future radio button widgets.

--- src/includes/forms/form-element.inc.js
+++ src/includes/forms/form-element.inc.js
@@ -14,7 +14,22 @@ dg.FormElement = function(name, element, form) {
   if (arguments.length) { dg.FormElementPrepare(this, arguments); }
 };
 
+/**
+ *
+ * @param FormElement
+ * @param args
+ */
 dg.FormElementPrepare = function(FormElement, args) {
+
+  // List of overrides for future reference.
+  // Radio
+  // Radios
+
+  // Grab defaults.
+  dg._formElementPrepare(FormElement, args);
+
+};
+dg._formElementPrepare = function(FormElement, args) {
   FormElement.name = args[0];
   FormElement.element = args[1]; // Holds the form element JSON object provided by the form builder.
   FormElement.form = args[2];
@@ -33,12 +48,20 @@ dg.FormElement.prototype.id = function() { return this.element ? this.element._a
  */
 dg.FormElement.prototype.getForm = function() { return this.form; };
 
+dg.FormElement.prototype.getFormDomId = function() {
+  return this.getForm().getFormDomId();
+};
+
 /**
  *
  * @param prop
  * @returns {null}
  */
 dg.FormElement.prototype.get = function(prop) { return typeof this[prop] ? this[prop] : null; };
+
+dg.FormElement.prototype.getQuerySelector = function() {
+  return '#' + this.getFormDomId() + ' #' + this.id();
+};
 
 /**
  * Given an element name, this will return its render element.
@@ -49,6 +72,14 @@ dg.FormElement.prototype.getElement = function(name) {
   return typeof this.get('element')[name] ? this.get('element')[name] : null;
 };
 
+dg.FormElement.prototype.getDomElement = function() {
+  return document.querySelector(this.getQuerySelector());
+};
+
+dg.FormElement.prototype.validateValue = function(value) {
+  return !jDrupal.isEmpty(value);
+};
+
 /**
  *
  * @returns {*}
@@ -57,7 +88,7 @@ dg.FormElement.prototype.valueCallback = function() {
   var self = this;
   return new Promise(function(ok, err) {
     var value = null;
-    var el = document.getElementById(self.id());
+    var el = self.getDomElement();
     if (el) { value = el.value; }
     ok({
       name: self.get('name'),
