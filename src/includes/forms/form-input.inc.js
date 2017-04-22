b54b579dc432d5329a85a29e53584baa90023530
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-04-21 22:40:24 -0400

    Support render element basics for checkbox options, not just strings. Add theme_radio() and theme_radios(), both probably a little hacky still.

--- src/includes/forms/form-input.inc.js
+++ src/includes/forms/form-input.inc.js
@@ -6,6 +6,17 @@ dg.theme_actions = function(variables) {
   }
   return html;
 };
+
+/**
+ * Renders a checkbox.
+ * @param variables
+ *  _title {string} The title to use on the checkbox label.
+ *  _prefix {string} HTML to place before the input.
+ *  _suffix {string} HTML to place before the input (or label).
+ *  _default_value {*} Optional
+ *  _attributes {Object}
+ * @returns {string}
+ */
 dg.theme_checkbox = function(variables) {
   variables._attributes.type = 'checkbox';
   if (!variables._attributes.id) { variables._attributes.id = 'checkbox-' + jDrupal.userPassword(); }
@@ -13,19 +24,36 @@ dg.theme_checkbox = function(variables) {
   var label = variables._title ?
     ' <label for="' + variables._attributes.id + '">' + variables._title + '</label>' :
     '';
-  return '<input ' + dg.attributes(variables._attributes) + ' />' + label;
+  var prefix = variables._prefix ? variables._prefix : '';
+  var suffix = variables._suffix ? variables._suffix : '';
+  return prefix + '<input ' + dg.attributes(variables._attributes) + ' />' + label + suffix;
 };
+
+/**
+ * Renders a set of checkboxes.
+ * @param variables
+ *   _options {object}
+ *     123: 'foo',
+ *     456: 'bar',
+ *     789: {
+ *       _title: 'baz',
+ *       _prefix: '<div class="my-checkbox">',
+ *       _suffix: '<div>',
+ *     }
+ * @returns {string}
+ */
 dg.theme_checkboxes = function(variables) {
   var html = '';
   for (var value in variables._options) {
     if (!variables._options.hasOwnProperty(value)) { continue; }
     var label = variables._options[value];
-    var checkboxVariables = {
-      _title: !jDrupal.isEmpty(label) ? label : value,
-      _attributes: {
-        value: value
-      }
-    };
+    var checkboxVariables = typeof label !== 'object' ? {
+      _title: !jDrupal.isEmpty(label) ? label : value
+    } : label;
+    if (!checkboxVariables._attributes) { checkboxVariables._attributes = {}; }
+    if (typeof checkboxVariables._attributes.value === 'undefined') {
+      checkboxVariables._attributes.value = value;
+    }
     if (variables._default_value && variables._default_value[value]) {
       checkboxVariables._default_value = variables._default_value[value];
     }
@@ -48,6 +76,34 @@ dg.theme_number = function(variables) {
 dg.theme_password = function(variables) {
   variables._attributes.type = 'password';
   return '<input ' + dg.attributes(variables._attributes) + ' />';
+};
+dg.theme_radio = function(variables) {
+  variables._attributes.type = 'radio';
+  var title = variables._title ? variables._title : null;
+  var titleAttributes = variables._title_attributes ? variables._title_attributes : {};
+  if (!titleAttributes.for) { titleAttributes.for = variables._attributes.id; }
+  var html = '<input ' + dg.attributes(variables._attributes) + '/>';
+  if (title) { html += '<label ' + dg.attributes(titleAttributes) + '>' + title + '</label>'; }
+  return html;
+};
+dg.theme_radios = function(variables) {
+  var html = '';
+  for (var value in variables._options) {
+    if (!variables._options.hasOwnProperty(value)) { continue; }
+    var label = variables._options[value];
+    var radioVariables = {
+      _title: !jDrupal.isEmpty(label) ? label : value,
+      _attributes: {
+        value: value,
+        name: variables._attributes.name
+      }
+    };
+    if (variables._default_value && variables._default_value[value]) {
+      checkboxVariables._default_value = variables._default_value[value];
+    }
+    html += '<div>' + dg.theme('radio', radioVariables) + '</div>';
+  }
+  return '<div ' + dg.attributes(variables._attributes) + '>' + html + '</div>';
 };
 dg.theme_select = function(variables) {
   var options = '';
