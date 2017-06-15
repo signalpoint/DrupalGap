5181c79f93f86ffb59be7e0b0e520bf196482d0f
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-06-15 00:22:49 -0400

    Add hasRole() and isAdmin().

--- src/includes/user.inc.js
+++ src/includes/user.inc.js
@@ -1,2 +1,4 @@
 dg.currentUser = function() { return jDrupal.currentUser(); };
 dg.userPassword = function() { return jDrupal.userPassword.apply(jDrupal, arguments); };
+dg.hasRole = function(role) { return jDrupal.currentUser().hasRole(role); };
+dg.isAdmin = function() { return dg.hasRole('administrator'); };
