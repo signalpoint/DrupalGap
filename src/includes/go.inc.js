73e08e4f6541158b666cd44d2d2c3b45fb08fb34
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-07-12 11:06:35 -0400

    Add getDestination(), getDestinationPath() and getQueryString(). Allow options in goto().

--- src/includes/go.inc.js
+++ src/includes/go.inc.js
@@ -1,4 +1,31 @@
-dg.goto = function(path) {
+/**
+ *
+ * @param path {String} The router path to navigate to.
+ * @param options {Object}
+ *  _query: @see dg.theme_link()
+ */
+dg.goto = function(path, options) {
+  path += dg.getQueryString(options);
   this.router.navigate(path);
-  //this.router.check('/' + path);
+};
+
+dg.getDestination = function() {
+  return { destination: dg.getDestinationPath() };
+};
+
+dg.getDestinationPath = function() {
+  return dg.arg().join('/');
+};
+
+dg.getQueryString = function(variables) {
+  if (variables._query) {
+    var query = variables._query;
+    var queries = [];
+    for (var name in query) {
+      if (!query.hasOwnProperty(name)) { continue; }
+      queries.push(name + '=' + query[name]);
+    }
+    if (queries.length) { return '?' + queries.join('&'); }
+  }
+  return '';
 };
