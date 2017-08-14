ab307ec06f20f14a0368d757d32c500066ade5fc
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2017-08-14 17:23:23 -0400

    #455 Add sweeping improvements to file form element in web app mode, not ready yet though.

--- src/includes/form/form.file.js
+++ src/includes/form/form.file.js
@@ -1,11 +1,90 @@
+var dgFile = {
+
+  error: function(xhr, status, msg) {},
+
+  parseFid: function(fid) { return parseInt(fid); },
+  getFileElementNamesFromForm: function(form) {
+    var names = [];
+    if (form.elements) {
+      for (var name in form.elements) {
+        if (!form.elements.hasOwnProperty(name)) { continue; }
+        var element = form.elements[name];
+        if (!element.type || element.type != 'file') { continue; }
+        names.push(name);
+        break;
+      }
+    }
+    return names.length ? names : null;
+  },
+
+  _pendingFileIds: [],
+  getPendingFileIds: function() { return dgFile._pendingFileIds; },
+  setPendingFileIds: function(fileIds) { dgFile._pendingFileIds = fileIds; },
+  hasPendingFileIds: function() { return dgFile.getPendingFileIds().length; },
+  isPendingFileId: function(fid) {
+    return in_array(dgFile.parseFid(fid), dgFile.getPendingFileIds());
+  },
+  addToPendingFileIds: function(fid) {
+    fid = dgFile.parseFid(fid);
+    if (!dgFile.isPendingFileId(fid)) {
+      dgFile.getPendingFileIds().push(fid);
+      console.log('added to pending list: ' + fid);
+    }
+  },
+  removeFromPendingFileIds: function(fid) {
+    fid = dgFile.parseFid(fid);
+    var fileIds = dgFile.getPendingFileIds();
+    console.log('removeFromPendingFileIds', fid, dgFile.getPendingFileIds());
+    fileIds = fileIds.filter(function(e) { return e !== fid });
+    dgFile.setPendingFileIds(fileIds);
+    console.log('removeFromPendingFileIds', fid, dgFile.getPendingFileIds());
+  },
+
+  //_pendingDelete: {},
+  //getPendingDelete: function() { return this._pendingDelete; },
+  //addToPendingDelete: function(entityType, entityId) {
+  //  var pending = this._pendingDelete;
+  //  if (!pending[entityType]) { pending[entityType] = []; }
+  //  pending[entityType].push(entityId);
+  //},
+  //hasPendingDelete: function(entityType, entityId) {
+  //  var isPending = false;
+  //  var pending = this.getPendingDelete();
+  //  $.each(pending, function(_entityType, ids) {
+  //    if (entityType == _entityType && in_array(entityId, ids)) {
+  //      isPending = true;
+  //      return false;
+  //    }
+  //  });
+  //  return isPending;
+  //},
+  //pendingDeleteEntityTypes: function() {
+  //  var entityTypes = [];
+  //  var pending = this.getPendingDelete();
+  //  $.each(pending, function(entityType, ids) {
+  //    entityTypes.push(entityType);
+  //  });
+  //  return entityTypes.length ? entityTypes: null;
+  //}
+
+};
+
+
 /**
  * Themes a file input.
  * @param variables
  * @returns {string}
  */
 function theme_file(variables) {
+  //console.log('theme_file', variables);
   var attrs = variables.attributes;
 
+  // Generate an attribute id if one wasn't provided.
+  if (!attrs.id) { attrs.id = 'file-widget-' + user_password(); }
+
+  var wrapperId = attrs.id + '-wrapper';
+
+  var html = '<div id="' + wrapperId + '" class="file-widget-wrapper">';
 
   if (variables.file) {
 
@@ -20,23 +99,24 @@ function theme_file(variables) {
       attributes: {
         'data-icon': 'delete',
         'data-iconpos': 'notext',
-        onclick: 'dgFileWidgetRemoveOnclick(' + file.fid + ')'
+        onclick: 'dgFileWidgetRemoveOnclick(' + file.fid + ')',
+        wrapperId: wrapperId
       }
     });
-    return deleteBtn + preview;
+    html += deleteBtn + preview;
 
   }
   else {
 
     // There is no default file...
 
-    // Generate some DOM ids.
-    if (!attrs.id) { attrs.id = 'file-widget-' + user_password(); }
     var previewId = attrs.id + '-preview';
 
     // Input widget.
     if (!attrs.type) { attrs.type = 'file'; }
-    if (!attrs.onchange) { attrs.onchange = "dgFileWidgetPreviewFile('" + attrs.id + "', '" + previewId + "')"; }
+    if (!attrs.onchange) {
+      attrs.onchange = "dgFileWidgetPreviewFile('" + wrapperId + "', '" + attrs.id + "', '" + previewId + "')";
+    }
     attrs.class += ' file-widget ';
     var input = '<input ' + drupalgap_attributes(attrs) + '/>';
     var previewAttrs = {
@@ -47,13 +127,15 @@ function theme_file(variables) {
     // Preview widget.
     var preview = '<img ' + drupalgap_attributes(previewAttrs) + '>';
 
-    return input + preview;
+    html += input + preview;
 
   }
 
+  return html + '</div>';
+
 }
 
-function dgFileWidgetPreviewFile(inputId, previewId) {
+function dgFileWidgetPreviewFile(wrapperId, inputId, previewId) {
 
   // @TODO if the user saves the file to Drupal, then backs out of the page or doesn't submit the form, then the file
   // space and file_managed entry will be wasteful and a ghost.
@@ -63,61 +145,90 @@ function dgFileWidgetPreviewFile(inputId, previewId) {
 
   // Grab the file input widget.
   var file = document.querySelector('#' + inputId).files[0];
-  console.log('file', file);
+  if (!file) { return; }
+  console.log('file input', file);
 
-  // Get ready to load the file...
-  var reader  = new FileReader();
-  reader.addEventListener("load", function () {
+  // Prepare to read the file.
+  var dgFileRead = function() {
 
-    // The file has been loaded...
+    // Get ready to load the file...
+    var reader  = new FileReader();
+    reader.addEventListener("load", function () {
 
-    // Preview the image.
-    preview.src = reader.result;
-    preview.height = 64;
+      // The file has been loaded...
 
-    // Build the JSON deliverable.
-    var fileData = {
-      file: reader.result,
-      filename: file.name,
-      filepath: "public://" + file.name
-    };
+      // Preview the image.
+      preview.src = reader.result;
+      preview.height = 64;
 
-    // Prepare for any errors.
-    var notOk = function(xhr, status, msg) { drupalgap_alert(msg); };
+      // Build the JSON deliverable.
+      var fileData = {
+        file: reader.result,
+        filename: file.name,
+        filepath: "public://" + file.name
+      };
 
-    // Save the file to Drupal...
-    file_save(fileData, {
+      // Save the file to Drupal...
+      file_save(fileData, {
+        success: function(result) {
+          console.log('file_save', result);
+          if (result.fid) {
+
+            var fid = result.fid;
+            dgFile.addToPendingFileIds(fid);
+
+            // Set the file id onto the input form element.
+            $('#' + inputId).attr('fid', fid);
+
+          }
+          else { dgFile.error(null, null, t('There was a problem saving the file.')); }
+        },
+        error: dgFile.error
+      });
+
+    }, false);
+
+    // Finally, load the file chosen by the user and circle back to the "load" event listener.
+    if (file) { reader.readAsDataURL(file); }
+
+  };
+
+  // If they previously selected a file for preview, then delete it from Drupal's file management since it will be
+  // unused.
+  var fid = $('#' + inputId).attr('fid');
+  if (fid) {
+    console.log('fid', fid);
+    file_delete(fid, {
       success: function(result) {
-        if (result.fid) {
-
-          // Set the file id onto the input form element.
-          $('#' + inputId).attr('value', result.fid);
-
-        }
-        else { notOk(null, null, t('There was a problem saving the file.')); }
+        dgFile.removeFromPendingFileIds(fid);
+        dgFileRead();
       },
-      error: notOk
+      error: function(xhr, status, msg) { drupalgap_alert(msg); }
     });
-
-  }, false);
-
-  // Finally, load the file chosen by the user and circle back to the "load" event listener.
-  if (file) { reader.readAsDataURL(file); }
+  }
+  else { dgFileRead(); }
 }
 
 function dgFileWidgetRemoveOnclick(fid) {
-  drupalgap_confirm(t('Are you sure you want to remove this file?'), {
-    confirmCallback: function(button) {
-      console.log(button);
-      if (button === 1) {
-        file_delete(fid, {
-          success: function(result) {
-            console.log(result);
-          },
-          error: function(xhr, status, msg) { drupalgap_alert(msg); }
+  file_delete(fid, {
+    success: function(result) {
+      console.log(result);
+
+      // When editing an existing entity, the file will be marked as used by Drupal's file management
+      // system...
+      if (result.file) {
+        $.each(result.file, function(entityType, entities) {
+          $.each(entities, function(entityId) {
+            // If a file is in use, we can't delete it. However, it appears that if an entity update call
+            // removes a reference to that file id from an e.g. image field, then Drupal automatically deletes
+            // the file after the entity is updated.
+
+            //dgFile.addToPendingFileIds(fid);
+          });
         });
       }
-      else if (button === 2) { /* cancel */ }
-    }
+
+    },
+    error: function(xhr, status, msg) { drupalgap_alert(msg); }
   });
 }
--- src/modules/file/file.js
+++ src/modules/file/file.js
@@ -1,4 +1,74 @@
 /**
+ * Implements hook_form_alter().
+ */
+function file_form_alter(form, form_state, form_id) {
+
+  // Make sure we have at least one file element on the form.
+  var fileElements = dgFile.getFileElementNamesFromForm(form);
+  if (!fileElements) { return; }
+  console.log('file_form_alter', form, fileElements);
+
+  // Add a custom form submit handler to any form that has a file input element.
+  form.submit.push('file_form_submit_handler');
+
+  // Attach a value callback to each file input element, if one isn't set already.
+  for (var i = 0; i < fileElements.length; i++) {
+    var name = fileElements[i];
+    var element = form.elements[name];
+    if (!element.value_callback) { element.value_callback = 'file_value_callback'; }
+  }
+
+}
+
+function file_value_callback(id, element) {
+  var fid = $('#' + element.id).attr('fid');
+  return fid ? {
+    fid: fid,
+    display: 1
+  } : null;
+}
+
+function file_form_submit_handler(form, form_state) {
+  console.log('file_form_submit_handler', form_state.values);
+
+  // For each file input element that has a file id, remove that file id from the pending list.
+  var fileElements = dgFile.getFileElementNamesFromForm(form);
+  if (!fileElements) { return; }
+  for (var i = 0; i < fileElements.length; i++) {
+    var name = fileElements[i];
+    var element = form.elements[name];
+    var fid = form_state.values[name];
+    if (fid == '') { continue; }
+    console.log('removing fid from submit', fid);
+    dgFile.removeFromPendingFileIds(fid);
+  }
+}
+
+/**
+ * Implements hook_drupalgap_goto_post_process().
+ */
+function file_drupalgap_goto_post_process(path) {
+
+  // If there are any pending file ids when the app is changing pages, delete them from Drupal and remove their id
+  // from the dgFile pending tracker.
+  if (dgFile.hasPendingFileIds()) {
+    var fileIds = dgFile.getPendingFileIds().slice();
+    for (var i = 0; i < fileIds.length; i++) {
+      var fid = fileIds[i];
+      console.log('removing fid from goto preprocess', fid);
+      dgFile.removeFromPendingFileIds(fid);
+      file_delete(fid, {
+        success: function(result) {
+          console.log('deleted file from drupal');
+        },
+        error: dgFile.error
+      });
+    }
+  }
+
+}
+
+/**
  * Implements hook_field_formatter_view().
  */
 function file_field_formatter_view(entity_type, entity, field, instance, langcode, items, display) {
