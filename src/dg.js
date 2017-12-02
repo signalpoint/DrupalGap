d2110299dfdc4e296e0c1e45e1026eead5819718
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-12-01 21:58:08 -0500

    Abstract out dg.getModuleRoutes() and use it in dg.bootstrap(). Remove old commented out code.

--- src/dg.js
+++ src/dg.js
@@ -8,7 +8,8 @@ var dg = {
   _title: '', // The current page title,
   _continued: false, // A marker to indicate when dg.continue() has been called at least once
   _postRenderMax: 128, // The max allowed post renders functions in the queue.
-  _messages: [], // An array of individual message render elements to be displayed within the messages block.
+  _libraries: {}, // Tracks which libraries have been loaded.
+  _messages: [] // An array of individual message render elements to be displayed within the messages block.
 };
 // @TODO prefixing all properties above with an underscore, then use dg.get() throughout the SDK
 var drupalgap = dg;
@@ -38,10 +39,10 @@ dg.continue = function() {
 
 // Device ready.
 dg.deviceready = function() {
-  // @TODO, we'll probably want a hook_bootstrap() that can resolve with a Promise.
+
   dg.bootstrap();
 
-  // Invoke hook_deviceready()...
+  // Invoke hook_deviceready().
   jDrupal.moduleInvokeAll('deviceready').then(function() {
 
     // If no one implemented the hook, continue gracefully.
@@ -53,19 +54,6 @@ dg.deviceready = function() {
 
   });
 };
-//dg.devicereadyGood = function(data) {
-//
-//  // Pull out any important data from the Connect resource results.
-//  // @TODO move this to a jDrupal connect post processor
-//  if (data) {
-//    for (var d in data.drupalgap) {
-//      if (!data.drupalgap.hasOwnProperty(d)) { continue; }
-//      drupalgap[d] = data.drupalgap[d];
-//    }
-//  }
-//
-//
-//};
 
 // Bootstrap.
 dg.bootstrap = function() {
@@ -79,27 +67,48 @@ dg.bootstrap = function() {
   // module.
   var modules = jDrupal.modulesLoad();
   for (var module in modules) {
-    if (!modules.hasOwnProperty(module) || !modules[module].routing) { continue; }
-    var routes = modules[module].routing();
-    if (!routes) { continue; }
-    for (route in routes) {
-      if (!routes.hasOwnProperty(route)) { continue; }
-      var item = routes[route];
-      item.key = route;
-      dg.router.add(item);
-      var baseRoute = dg.router.getBaseRoute(item);
-      if (baseRoute) {
-        dg.router.saveAsChildRoute(item,baseRoute);
+    if (!modules.hasOwnProperty(module)) { continue; }
+
+    // ROUTING - Gather routes provided by modules.
+    var routes = dg.getModuleRoutes(module);
+    if (routes) {
+      for (var route in routes) {
+        if (!routes.hasOwnProperty(route)) { continue; }
+        var item = routes[route];
+        item.key = route;
+        dg.router.add(item);
+        var baseRoute = dg.router.getBaseRoute(item);
+        if (baseRoute) {
+          dg.router.saveAsChildRoute(item, baseRoute);
+        }
       }
     }
+
+    // LIBRARIES - Gather libraries provided by modules
+    //var libraries = dg.getModuleLibraries(module);
+    //if (libraries) {
+    //  console.log(module + ' libraries', libraries);
+    //  for (var libraryName in libraries) {
+    //    if (!libraries.hasOwnProperty(libraryName)) { continue; }
+    //    var library = libraries[libraryName];
+    //  }
+    //}
+
   }
+
+  // @TODO Give modules a chance to alter the libraries.
+  // In order to accommodate this, the dg._libraries object needs to be filled here during the bootstrap, but the
+  // libraries need to have a boolean property to indicate if it's been loaded or not, then dg.render() can set this
+  // boolean to true when the "onload" is complete.
+  //jDrupal.moduleInvokeAll('libraries_alter', dg.getModuleLibraries());
 
   // If there's no front page specified, set it to the default dg dashboard.
   if (!dg.config('front')) { dg.config('front', 'dg'); }
 
   // Load the theme, then the blocks, and then add a default route, and start listening.
   dg.themeLoad().then(function() {
-      var blocks = dg.blocksLoad();
+    //var blocks = dg.blocksLoad();
+    dg.blocksLoad();
     jDrupal.moduleInvokeAll('init').then(function() {
       dg.router.add(function() {}).listen();
     });
