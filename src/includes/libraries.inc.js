2bc085d7cc9a60bdddea2fb755145cc76254e48f
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-12-01 23:02:56 -0500

    #984 Add libraries.inc.js

---
+++ src/includes/libraries.inc.js
@@ -0,0 +1,107 @@
+dg.hasAttachments = function(content) { return !!content._attached; };
+dg.addAttachments = function(content) {
+
+  // Grab the libraries attached to the content, if any. Then iterate over each of the modules in the attachment to
+  // process their libraries.
+  var promises = [];
+  var attachments = content._attached;
+  for (var moduleName in attachments) {
+    if (!attachments.hasOwnProperty(moduleName)) { continue; }
+
+    // Grab the attachment, then load each library on the attachment, queueing up the promises returned by
+    // libraryLoad().
+    var attachment = attachments[moduleName];
+    for (var i = 0; i < attachment.length; i++) {
+      var libraryName = attachment[i];
+      promises.push(dg.libraryLoad(moduleName, libraryName));
+    }
+
+  }
+  return Promise.all(promises);
+
+};
+
+dg.libraryLoad = function(moduleName, libraryName) {
+  return new Promise(function(ok, err) {
+
+    // If the library has already been added, don't do it again, just resolve.
+    if (dg.libraryLoaded(moduleName, libraryName)) {
+      //console.log('library already added!', moduleName, libraryName);
+      ok();
+      return;
+    }
+    //console.log('library not yet added', moduleName, libraryName);
+
+    // Load up the library configuration from the module.
+    var library = dg.getModuleLibrary(moduleName, libraryName);
+    
+    // Specify which asset types we allow and count them.
+    var assetTypes = ['js', 'css'];
+    var assetTypesCount = assetTypes.length;
+    
+    // Iterate over the types of assets we allow...
+    var assetsLoading = 0;
+    for (var i = 0; i < assetTypesCount; i++) {
+      var assetType = assetTypes[i];
+
+      // If the module's library doesn't provide this asset type, then skip it.
+      if (!library[assetType]) { continue; }
+      
+      // The module's library has an asset(s) for this type, count how many.
+      var assetCount = library[assetType].length;
+
+      // Iterate over each asset of this type...
+      for (var j = 0; j < assetCount; j++) {
+        var asset = library[assetType][j];
+
+        // Set up our onload handler.
+        asset._attributes.onload = function() { // @TODO all library implementors to have their own onload too.
+
+          // Mark an asset as loaded.
+          assetsLoading--;
+          //console.log('done loading asset, assets left', assetsLoading);
+
+          // Once we're done loading all the assets, mark the library as loaded and resolve.
+          if (!assetsLoading) {
+            var libs = dg.getLibraries();
+            if (!libs[moduleName]) { libs[moduleName] = {}; }
+            if (!libs[moduleName][libraryName]) { libs[moduleName][libraryName] = {}; }
+            ok();
+          }
+
+        };
+
+        //console.log('loading asset', asset);
+        
+        // Add the asset's file to the head, then circle back to the onload handler.
+        assetsLoading++;
+        assetType == 'js' ? dg.addJs(asset) : dg.addCss(asset);
+
+      }
+
+    }
+
+  });
+};
+
+dg.libraryLoaded = function(moduleName, libraryName) {
+  var libs = dg._libraries;
+  return !!(libs[moduleName] && libs[moduleName][libraryName]);
+};
+
+dg.getLibraries = function() {
+  return dg._libraries;
+};
+
+dg.getModuleLibraries = function(moduleName) {
+  return dg.invoke(moduleName, 'libraries');
+};
+
+dg.getModuleLibrary = function(moduleName, libraryName) {
+  var libraries = dg.getModuleLibraries(moduleName);
+  return libraries[libraryName];
+};
+
+dg.getModuleRoutes = function(moduleName) {
+  return dg.invoke(moduleName, 'routing');
+};
