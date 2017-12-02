c1b3e9f11cc8e6e0ad8dd1600fd847af2c3ac701
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-12-01 22:44:10 -0500

    Improve code comments and readability of dg.js

--- src/dg.js
+++ src/dg.js
@@ -11,7 +11,7 @@ var dg = {
   _libraries: {}, // Tracks which libraries have been loaded.
   _messages: [] // An array of individual message render elements to be displayed within the messages block.
 };
-// @TODO prefixing all properties above with an underscore, then use dg.get() throughout the SDK
+// @TODO prefix all properties above with an underscore, then use dg.get() throughout the SDK.
 var drupalgap = dg;
 
 // Configuration setting defaults.
@@ -21,23 +21,23 @@ dg.settings = {
   blocks: {}
 };
 
-// Start.
+/**
+ * Starts the DrupalGap 8 SDK.
+ */
 dg.start = function() {
-  if (dg.isCompiled()) {
-    document.addEventListener('deviceready', dg.deviceready, false);
-  }
+
+  // If we're in a compiled app, attach our deviceready handler as cordova's deviceready listener, otherwise just call
+  // our deviceready handler to start the web application.
+  if (dg.isCompiled()) { document.addEventListener('deviceready', dg.deviceready, false); }
   else { dg.deviceready(); } // web-app
+
 };
 
-// Continue.
-dg.continue = function() {
-  // Force a check on the router (which is already listening at this point), to
-  // refresh the current page or navigate to the current path.
-  dg._continued = true;
-  dg.router.check(dg.router.getPath());
-};
-
-// Device ready.
+/**
+ * Handles the moment the device is ready by running the bootstrap, then invoking hook_bootstrap() to see if any
+ * developer wants to take over the "deviceready" process themselves, and often times no one does so by default we make
+ * a Connect call to jDrupal, and upon connection simply continue onward by forcing a route change to the front page.
+ */
 dg.deviceready = function() {
 
   dg.bootstrap();
@@ -55,59 +55,59 @@ dg.deviceready = function() {
   });
 };
 
-// Bootstrap.
+/**
+ * Assembles routes, loads up the theme and then tells the router to start listening for activity.
+ */
 dg.bootstrap = function() {
 
   dg.router.config({});
 
-  // Build the routes.
-  // @TODO turn route building into promises.
-  // @TODO turn the outer portion of this procedure into a re-usable function
-  // that can iterate over modules and call a specific function within that
-  // module.
+  // Assemble the routes provided by each module.
   var modules = jDrupal.modulesLoad();
   for (var module in modules) {
     if (!modules.hasOwnProperty(module)) { continue; }
 
-    // ROUTING - Gather routes provided by modules.
+    // Get the routes for this module.
     var routes = dg.getModuleRoutes(module);
     if (routes) {
       for (var route in routes) {
         if (!routes.hasOwnProperty(route)) { continue; }
+
+        // Get the route item and add it to the router.
         var item = routes[route];
         item.key = route;
         dg.router.add(item);
+
+        // If the route item has a base route specified, add the route item as a child of the base route.
         var baseRoute = dg.router.getBaseRoute(item);
-        if (baseRoute) {
-          dg.router.saveAsChildRoute(item, baseRoute);
-        }
+        if (baseRoute) { dg.router.saveAsChildRoute(item, baseRoute); }
       }
     }
 
-    // LIBRARIES - Gather libraries provided by modules
-    //var libraries = dg.getModuleLibraries(module);
-    //if (libraries) {
-    //  console.log(module + ' libraries', libraries);
-    //  for (var libraryName in libraries) {
-    //    if (!libraries.hasOwnProperty(libraryName)) { continue; }
-    //    var library = libraries[libraryName];
-    //  }
-    //}
-
   }
-
-  // @TODO Give modules a chance to alter the libraries.
-  // In order to accommodate this, the dg._libraries object needs to be filled here during the bootstrap, but the
-  // libraries need to have a boolean property to indicate if it's been loaded or not, then dg.render() can set this
-  // boolean to true when the "onload" is complete.
-  //jDrupal.moduleInvokeAll('libraries_alter', dg.getModuleLibraries());
 
   // If there's no front page specified, set it to the default dg dashboard.
   if (!dg.config('front')) { dg.config('front', 'dg'); }
 
+  // @TODO Give modules a chance to alter libraries provided by other modules.
+  // In order to accommodate this, the dg._libraries object needs to be filled here during the bootstrap, but the
+  // libraries need to have a boolean property to indicate if it's been loaded or not, then dg.render() can set this
+  // boolean to true when the "onload" is complete.
+
+  // LIBRARIES - Gather libraries provided by modules
+  //var libraries = dg.getModuleLibraries(module);
+  //if (libraries) {
+  //  console.log(module + ' libraries', libraries);
+  //  for (var libraryName in libraries) {
+  //    if (!libraries.hasOwnProperty(libraryName)) { continue; }
+  //    var library = libraries[libraryName];
+  //  }
+  //}
+
+  //jDrupal.moduleInvokeAll('libraries_alter', dg.getModuleLibraries());
+
   // Load the theme, then the blocks, and then add a default route, and start listening.
   dg.themeLoad().then(function() {
-    //var blocks = dg.blocksLoad();
     dg.blocksLoad();
     jDrupal.moduleInvokeAll('init').then(function() {
       dg.router.add(function() {}).listen();
@@ -116,6 +116,18 @@ dg.bootstrap = function() {
 
 };
 
+/**
+ * Forces a check on the router to refresh the current page or navigate to the current path.
+ */
+dg.continue = function() {
+
+  dg._continued = true;
+  dg.router.check(dg.router.getPath());
+};
+
+/**
+ * SPINNER
+ */
 dg.spinnerShow = function() {
   dg.spinner++;
   //if (dg.spinner == 1) { document.getElementById('dgSpinner').style.display = 'block'; }
