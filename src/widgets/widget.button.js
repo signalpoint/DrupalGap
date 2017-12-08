e20435e487ffefa4a74902177e0eb4529b93a608
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-12-08 00:20:08 -0500

    Add default 'type' attribute on widget.button.js

--- src/widgets/widget.button.js
+++ src/widgets/widget.button.js
@@ -5,5 +5,6 @@
  */
 dg.theme_button = function(variables) {
   if (!variables._value) { variables._value = ''; }
+  if (!variables._attributes.type) { variables._attributes.type = 'button'; }
   return '<button ' + dg.attributes(variables._attributes) + '>' + variables._value + '</button>';
 };
