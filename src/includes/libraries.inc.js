ca4b24d0e82d271101f60123e0989cb559e14267
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2017-12-12 21:47:49 -0500

    #984 A rough run at iterative .js asset loading for _attached libraries.

--- src/includes/libraries.inc.js
+++ src/includes/libraries.inc.js
@@ -1,5 +1,107 @@
 dg.hasAttachments = function(content) { return !!content._attached; };
+
 dg.addAttachments = function(content) {
+
+  var loadAttachment = function(moduleName, attachment) {
+
+    return new Promise(function(ok, err) {
+
+
+      //console.log('attachment', attachment);
+
+      var libraryCount = attachment.length;
+      var librariesProcessed = 0;
+      var libraryDelta = 0;
+      //console.log('attachment library count: ', libraryCount);
+
+      var loadLibrary = function() {
+
+        var libraryName = attachment[libraryDelta];
+
+        if (dg.libraryLoaded(moduleName, libraryName)) {
+          //console.log('library already loaded', moduleName, libraryName);
+          librariesProcessed++;
+          libraryDelta++;
+          ok();
+          return;
+        }
+
+        var library = dg.getModuleLibrary(moduleName, libraryName);
+        //console.log(moduleName, libraryName, library);
+
+        var jsAssetCount = library.js ? library.js.length : 0;
+        var cssAssetCount = library.css ? library.css.length : 0;
+        //console.log('jsAssetCount', jsAssetCount);
+        //console.log('cssAssetCount', cssAssetCount);
+
+        var jsAssetsProcessed = 0;
+        var jsDelta = 0;
+        var cssAssetsProcessed = 0;
+        var cssDelta = 0;
+
+        var loadJsAsset = function() {
+
+          var jsAsset = library.js[jsDelta];
+          //console.log('jsAsset', jsAsset);
+
+          // Set up our onload handler.
+          jsAsset._attributes.onload = function() {
+            jsAssetsProcessed++;
+            jsDelta++;
+            //console.log('loaded', jsAsset._attributes.src);
+            if (jsAssetsProcessed < jsAssetCount) {
+              //console.log('loaded a js asset, moving to the next');
+              loadJsAsset();
+            }
+            else {
+              //console.log('all done loading js assets, moving to the next library');
+              librariesProcessed++;
+              libraryDelta++;
+
+              // Mark the library as loaded.
+              var libs = dg.getLibraries();
+              if (!libs[moduleName]) { libs[moduleName] = {}; }
+              if (!libs[moduleName][libraryName]) { libs[moduleName][libraryName] = {}; }
+
+              // Give modules a chance to react to the completion of a loaded library..
+              jDrupal.moduleInvokeAll('library_onload', moduleName, libraryName);
+
+              if (librariesProcessed < libraryCount) {
+                loadLibrary();
+              }
+              else { ok(); }
+            }
+          };
+          dg.addJs(jsAsset);
+
+        };
+
+        if (jsAssetsProcessed < jsAssetCount) {
+          loadJsAsset();
+        }
+
+      };
+
+      loadLibrary();
+
+    });
+
+  };
+
+  var promises = [];
+  var attachments = content._attached;
+  for (var moduleName in attachments) {
+    if (!attachments.hasOwnProperty(moduleName)) { continue; }
+    var attachment = attachments[moduleName];
+    if (!jDrupal.isArray(attachment)) { continue; }
+    promises.push(loadAttachment(moduleName, attachment));
+  }
+  return Promise.all(promises);
+
+
+  
+
+  return;
 
   // Grab the libraries attached to the content, if any. Then iterate over each of the modules in the attachment to
   // process their libraries.
@@ -8,12 +110,40 @@ dg.addAttachments = function(content) {
   for (var moduleName in attachments) {
     if (!attachments.hasOwnProperty(moduleName)) { continue; }
 
-    // Grab the attachment, then load each library on the attachment, queueing up the promises returned by
-    // libraryLoad().
+    // Figure out what kind of attachment it is, a simple array of library names, or a more complex object with
+    // configuration.
     var attachment = attachments[moduleName];
+    console.log(attachment);
+    return;
+
+
     for (var i = 0; i < attachment.length; i++) {
       var libraryName = attachment[i];
-      promises.push(dg.libraryLoad(moduleName, libraryName));
+      dg.libraryLoadSync(moduleName, libraryName);
+    }
+    continue;
+
+
+
+
+    if (dg.isObject(attachment)) {
+      if (attachment.async === false) {
+        for (var i = 0; i < attachment.libraries.length; i++) {
+          var libraryName = attachment.libraries[i];
+          dg.libraryLoad(moduleName, libraryName, {
+            async: false
+          });
+        }
+      }
+    }
+    else {
+      if (dg.isString(attachment)) { attachment = [attachment]; } // Convert single strings into an array.
+
+      // load each library on the attachment, queueing up the promises returned by libraryLoad().
+      for (var i = 0; i < attachment.length; i++) {
+        var libraryName = attachment[i];
+        promises.push(dg.libraryLoad(moduleName, libraryName));
+      }
     }
 
   }
@@ -26,11 +156,11 @@ dg.libraryLoad = function(moduleName, libraryName) {
 
     // If the library has already been added, don't do it again, just resolve.
     if (dg.libraryLoaded(moduleName, libraryName)) {
-      //console.log('library already added!', moduleName, libraryName);
+      console.log('library already added!', moduleName, libraryName);
       ok();
       return;
     }
-    //console.log('library not yet added', moduleName, libraryName);
+    console.log('library not yet added', moduleName, libraryName);
 
     // Load up the library configuration from the module.
     var library = dg.getModuleLibrary(moduleName, libraryName);
@@ -73,7 +203,7 @@ dg.libraryLoad = function(moduleName, libraryName) {
 
           // Mark an asset as loaded.
           assetsLoading--;
-          //console.log('done loading asset, assets left: ' + assetsLoading);
+          console.log('done loading asset, assets left: ' + assetsLoading);
 
           // Once we're done loading all the assets...
           if (!assetsLoading) {
@@ -82,7 +212,6 @@ dg.libraryLoad = function(moduleName, libraryName) {
             var libs = dg.getLibraries();
             if (!libs[moduleName]) { libs[moduleName] = {}; }
             if (!libs[moduleName][libraryName]) { libs[moduleName][libraryName] = {}; }
-
 
             // Give modules a chance to react to the completion of a loaded library..
             jDrupal.moduleInvokeAll('library_onload', moduleName, libraryName);
@@ -93,10 +222,10 @@ dg.libraryLoad = function(moduleName, libraryName) {
 
         };
 
-        //console.log('loading asset', asset);
+        console.log('loading asset', asset);
         
         // Add the asset's file to the head, then circle back to the onload handler.
-        //console.log('LOADING: ' + moduleName + '/' + libraryName);
+        console.log('LOADING: ' + moduleName + '/' + libraryName);
         assetType == 'js' ? dg.addJs(asset) : dg.addCss(asset);
 
       }
