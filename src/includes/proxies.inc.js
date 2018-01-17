9572a9394ed2322d4d6cbe3fdb81b56dba5d05c1
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-01-17 10:34:30 -0500

    Add dg.salt() as a shortcut proxy to jDrupal.userPassword().

--- src/includes/proxies.inc.js
+++ src/includes/proxies.inc.js
@@ -1,1 +1,7 @@
 dg.isArray = function(thing) { return jDrupal.isArray(thing); };
+
+/**
+ * Returns a random string of alpha numeric characters.
+ * @see jDrupal.userPassword()
+ */
+dg.salt = function() { return jDrupal.userPassword.apply(this, arguments); };
