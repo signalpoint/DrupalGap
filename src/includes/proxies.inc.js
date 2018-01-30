fb0ecf73d874c7ea9bd5ba59d7c18ae4ad77c850
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-01-30 00:09:00 -0500

    Add more proxies to jDrupal. Deprecate userPassword().

--- src/includes/proxies.inc.js
+++ src/includes/proxies.inc.js
@@ -1,7 +1,30 @@
+/**
+ * @see jDrupal.isArray()
+ */
 dg.isArray = function(thing) { return jDrupal.isArray(thing); };
+
+/**
+ * @see jDrupal.isEmpty()
+ */
+dg.isEmpty = function(thing) { return jDrupal.isEmpty(thing); };
+
+/**
+ * @see jDrupal.isInt()
+ */
+dg.isInt = function(thing) { return jDrupal.isInt(thing); };
 
 /**
  * Returns a random string of alpha numeric characters.
  * @see jDrupal.userPassword()
  */
 dg.salt = function() { return jDrupal.userPassword.apply(this, arguments); };
+
+dg.currentUser = function() { return jDrupal.currentUser(); };
+
+/**
+ * @deprecated
+ */
+dg.userPassword = function() {
+  console.log('dg.userPassword() is deprecated, use dg.salt() instead.');
+  return dg.salt.apply(this, arguments);
+};
