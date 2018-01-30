95d3d26c18f0fbd8041dea885eca42d10c5d1d2a
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-01-30 00:09:30 -0500

    Remove some functions to were moved to proxies.inc.js

--- src/includes/user.inc.js
+++ src/includes/user.inc.js
@@ -1,12 +1,2 @@
-dg.currentUser = function() { return jDrupal.currentUser(); };
-
-/**
- * @deprecated
- */
-dg.userPassword = function() {
-  console.log('dg.userPassword() is deprecated, use dg.salt() instead.');
-  return dg.salt.apply(this, arguments);
-};
-
 dg.hasRole = function(role) { return jDrupal.currentUser().hasRole(role); };
 dg.isAdmin = function() { return dg.hasRole('administrator'); };
