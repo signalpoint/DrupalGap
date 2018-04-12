9c4a62192d55bf26a0a0d5d2cc1bfd2a307f396f
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2018-04-11 21:38:15 -0400

    Fix dg.imageStyleUrl() for compiled mode so it doesn't double prefix the site path.

--- src/widgets/widget.image-style.js
+++ src/widgets/widget.image-style.js
@@ -3,14 +3,23 @@ dg.theme_image_style = function(vars) {
   return dg.theme_image(vars);
 };
 
+/**
+ * Given a style name and image URI, this will return the full URL for the image.
+ * @param styleName {String} An image style name.
+ * @param path {String} An image URI (recommended) or path.
+ * @returns {*}
+ */
 dg.imageStyleUrl = function(styleName, path) {
-  var src = jDrupal.sitePath() + jDrupal.basePath() + path;
-  var common = 'sites/default/files/styles/' + styleName + '/public/';
-  return src.indexOf('public://') != -1 ?
-      src.replace(
-          'public://',
-          dg.isCompiled() ?
-            jDrupal.sitePath() + '/' + common :
-              common
-      ) : src;
+  var look = 'public://';
+  var isUri = path.indexOf(look) != -1;
+  var sitePath = jDrupal.sitePath();
+  var basePath = jDrupal.basePath();
+  if (isUri) {
+    var common = 'sites/default/files/styles/' + styleName + '/public/';
+    var fullPath = basePath + common;
+    return (
+            dg.isCompiled() ? sitePath + fullPath : fullPath
+    ) + path.replace(look, '');
+  }
+  else { return sitePath + basePath + path; }
 };
