495df2c008cbbc100504a44703e3d41d7127cfbd
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2021-02-09 22:20:15 -0500

    Update gulp syntax a bit, still having problems with watch not working after the first time on some files

--- gulpfile.js
+++ gulpfile.js
@@ -7,74 +7,72 @@ var gulp = require('gulp'),
     gp_uglify = require('gulp-uglify');
 
 var moduleSrc = [
-  'src/dg.js',
 
-  'src/includes/proxies.inc.js',
-  'src/includes/block.inc.js',
-  'src/includes/common.inc.js',
-  'src/includes/entity.inc.js',
+  './src/*.js',
 
-  'src/includes/forms/form.inc.js',
-  'src/includes/forms/form-*.inc.js',
-  'src/includes/forms/elements/element.*.js',
+  './src/includes/proxies.inc.js',
+  './src/includes/block.inc.js',
+  './src/includes/common.inc.js',
+  './src/includes/entity.inc.js',
 
-  'src/includes/field-definition.inc.js',
-  'src/includes/field-form-mode.inc.js',
-  'src/includes/field-formatter.inc.js',
-  'src/includes/field-item-list-interface.inc.js',
-  'src/includes/field-widget.inc.js',
+  './src/includes/forms/form.inc.js',
+  './src/includes/forms/form-*.inc.js',
+  './src/includes/forms/elements/element.*.js',
 
-  'src/includes/dom.inc.js',
-  'src/includes/go.inc.js',
-  'src/includes/libraries.inc.js',
-  'src/includes/messages.inc.js',
-  'src/includes/module.inc.js',
-  'src/includes/region.inc.js',
-  'src/includes/render.inc.js',
-  'src/includes/rest.inc.js',
-  'src/includes/router.inc.js',
-  'src/includes/theme.inc.js',
-  'src/includes/user.inc.js',
-  'src/includes/views.inc.js',
+  './src/includes/field-definition.inc.js',
+  './src/includes/field-form-mode.inc.js',
+  './src/includes/field-formatter.inc.js',
+  './src/includes/field-item-list-interface.inc.js',
+  './src/includes/field-widget.inc.js',
 
-  'src/modules/admin/admin.js',
-  'src/modules/core/core.js',
-  'src/modules/core/core.field-formatters.js',
-  'src/modules/core/core.field-widgets.js',
-  'src/modules/core/core.form-widgets.js',
-  'src/modules/core/core.theme.js',
-  'src/modules/image/image.js',
-  'src/modules/image/image.field-formatters.js',
-  'src/modules/node/node.js',
-  'src/modules/node/node.forms.js',
-  'src/modules/system/*.js',
-  'src/modules/system/blocks/block.*.js',
-  'src/modules/text/text.js',
-  'src/modules/text/text.field-formatters.js',
-  'src/modules/text/text.field-widgets.js',
-  'src/modules/user/user.js',
-  'src/modules/user/user.forms.js',
+  './src/includes/dom.inc.js',
+  './src/includes/go.inc.js',
+  './src/includes/libraries.inc.js',
+  './src/includes/messages.inc.js',
+  './src/includes/module.inc.js',
+  './src/includes/region.inc.js',
+  './src/includes/render.inc.js',
+  './src/includes/rest.inc.js',
+  './src/includes/router.inc.js',
+  './src/includes/theme.inc.js',
+  './src/includes/user.inc.js',
+  './src/includes/views.inc.js',
 
-  'src/widgets/widget.*.js'
+  './src/modules/admin/admin.js',
+  './src/modules/core/core.js',
+  './src/modules/core/core.field-formatters.js',
+  './src/modules/core/core.field-widgets.js',
+  './src/modules/core/core.form-widgets.js',
+  './src/modules/core/core.theme.js',
+  './src/modules/image/image.js',
+  './src/modules/image/image.field-formatters.js',
+  './src/modules/node/node.js',
+  './src/modules/node/node.forms.js',
+  './src/modules/system/*.js',
+  './src/modules/system/blocks/block.*.js',
+  './src/modules/text/text.js',
+  './src/modules/text/text.field-formatters.js',
+  './src/modules/text/text.field-widgets.js',
+  './src/modules/user/user.js',
+  './src/modules/user/user.forms.js',
+
+  './src/widgets/widget.*.js'
+
 ];
 
 // Minify JavaScript
 function minifyJs() {
   console.log('compressing drupalgap.js...');
-  if (minify) {
-    var moduleJs = gulp.src(moduleSrc)
+  var moduleJs = gulp.src(moduleSrc)
       .pipe(gp_concat('drupalgap.js'))
       .pipe(gulp.dest('./'));
-      console.log('compressing drupalgap.min.js...');
+  if (minify) {
+    console.log('compressing drupalgap.min.js...');
     return moduleJs.pipe(gp_rename('drupalgap.min.js'))
       .pipe(gp_uglify())
       .pipe(gulp.dest('./'));
   }
-  else {
-    return gulp.src(moduleSrc)
-      .pipe(gp_concat('drupalgap.js'))
-      .pipe(gulp.dest('./'));
-  }
+  return moduleJs;
 }
 gulp.task(minifyJs);
 
