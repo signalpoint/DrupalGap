c6619612739d225453b1d067cecf38c65f0cd415
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2021-12-18 22:26:53 -0500

    Add getUrlParameter(), setUrlParameter() and _removeUrlParameter().

--- src/includes/go.inc.js
+++ src/includes/go.inc.js
@@ -61,3 +61,33 @@ dg.removeQueryString = function(path) {
   var queryIndex = path.indexOf('?');
   return queryIndex != -1 ? path.substr(0, queryIndex) : path;
 };
+
+
+dg.getUrlParameter = function(key) {
+  var urlParams = new URLSearchParams(dg.getQueryString());
+  return urlParams.get(key);
+};
+
+// @see https://stackoverflow.com/a/48889338/763010
+dg.setUrlParameter = function(key, value) {
+  if (history.pushState) {
+    var currentUrl = dg._removeUrlParameter(window.location.href, key);
+    var queryStart = currentUrl.indexOf('?') !== -1 ? '&' : '?';
+    var newUrl = currentUrl + queryStart + key + '=' + value;
+    if (!value) { newUrl = currentUrl; }
+    window.history.replaceState({ path: newUrl }, '', newUrl);
+  }
+};
+
+dg._removeUrlParameter = function(url, parameter) {
+  var urlparts = url.split('?');
+  if (urlparts.length >= 2) {
+    var prefix = encodeURIComponent(parameter) + '=';
+    var pars = urlparts[1].split(/[&;]/g);
+    for (var i = pars.length; i-- > 0;) {
+      if (pars[i].lastIndexOf(prefix, 0) !== -1) { pars.splice(i, 1); }
+    }
+    return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : "");
+  }
+  else { return url; }
+};
