4ce6908d4433bf17ee6c5bb723207c72ef1d14ac
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2024-04-10 14:40:18 +0000

    In theme_container(), add _type param and default it to div

--- src/widgets/widget.container.js
+++ src/widgets/widget.container.js
@@ -1,3 +1,10 @@
-dg.theme_container = function(variables) {
-  return '<div ' + dg.attrs(variables) + '>' + dg.render(variables._children) + '</div>';
+/**
+ *
+ * @param {type} vars
+ *  _type {String} The html element type to use, defaults to div.
+ * @returns {String}
+ */
+dg.theme_container = function(vars) {
+  var type = vars._type ? vars._type : 'div';
+  return '<' + type + ' ' + dg.attrs(vars) + '>' + dg.render(vars._children) + '</' + type + '>';
 };
