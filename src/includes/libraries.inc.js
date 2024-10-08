aee0e2ed76dcf4a1e10e7960462ed025d3600c58
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2024-10-08 17:10:59 +0000

    Add _attachments, clearAttachments(), setAttachment() and getAttachment(). Use setAttachment() in addAttachments(). Add data-module and data-library attributes to script and link elements in head. Support css with attachments. Add removeLibraries().

--- src/includes/libraries.inc.js
+++ src/includes/libraries.inc.js
@@ -1,3 +1,8 @@
+dg._attachments = {};
+dg.clearAttachments = function() { dg._attachments = {}; };
+dg.setAttachment = function(name, attachment) { dg._attachments[name] = attachment; }
+dg.getAttachment = function(name) { return dg._attachments[name]; }
+
 dg.hasAttachments = function(content) { return !!content._attached; };
 
 dg.addAttachments = function(content) {
@@ -6,20 +11,25 @@ dg.addAttachments = function(content) {
 
     return new Promise(function(ok, err) {
 
+//      console.log('loading attachment...', moduleName, attachment);
 
-      //console.log('attachment', attachment);
+      dg.setAttachment(moduleName, attachment);
 
       var libraryCount = attachment.length;
       var librariesProcessed = 0;
       var libraryDelta = 0;
-      //console.log('attachment library count: ', libraryCount);
 
       var loadLibrary = function() {
 
-        var libraryName = attachment[libraryDelta];
+        var libraryName = null;
+        var libraryIsObject = dg.isObject(attachment[libraryDelta]);
+
+        libraryName = libraryIsObject ?
+          attachment[libraryDelta].name :
+          attachment[libraryDelta];
 
         if (dg.libraryLoaded(moduleName, libraryName)) {
-          //console.log('library already loaded', moduleName, libraryName);
+          console.log('library already loaded', moduleName, libraryName);
           librariesProcessed++;
           libraryDelta++;
           ok();
@@ -27,12 +37,12 @@ dg.addAttachments = function(content) {
         }
 
         var library = dg.getModuleLibrary(moduleName, libraryName);
-        //console.log(moduleName, libraryName, library);
+//        console.log(moduleName, libraryName, library);
 
         var jsAssetCount = library.js ? library.js.length : 0;
         var cssAssetCount = library.css ? library.css.length : 0;
-        //console.log('jsAssetCount', jsAssetCount);
-        //console.log('cssAssetCount', cssAssetCount);
+//        console.log('jsAssetCount', jsAssetCount);
+//        console.log('cssAssetCount', cssAssetCount);
 
         var jsAssetsProcessed = 0;
         var jsDelta = 0;
@@ -42,19 +52,22 @@ dg.addAttachments = function(content) {
         var loadJsAsset = function() {
 
           var jsAsset = library.js[jsDelta];
-          //console.log('jsAsset', jsAsset);
+//          console.log('jsAsset', jsAsset);
+
+          jsAsset._attributes['data-module'] = moduleName;
+          jsAsset._attributes['data-library'] = libraryName;
 
           // Set up our onload handler.
           jsAsset._attributes.onload = function() {
             jsAssetsProcessed++;
             jsDelta++;
-            //console.log('loaded', jsAsset._attributes.src);
+//            console.log('loaded', jsAsset._attributes.src);
             if (jsAssetsProcessed < jsAssetCount) {
-              //console.log('loaded a js asset, moving to the next');
+//              console.log('loaded a js asset, moving to the next');
               loadJsAsset();
             }
             else {
-              //console.log('all done loading js assets, moving to the next library');
+//              console.log('all done loading js assets, moving to the next library');
               librariesProcessed++;
               libraryDelta++;
 
@@ -76,8 +89,52 @@ dg.addAttachments = function(content) {
 
         };
 
+        var loadCssAsset = function() {
+
+          var cssAsset = library.css[cssDelta];
+//          console.log('cssAsset', cssAsset);
+
+          cssAsset._attributes['data-module'] = moduleName;
+          cssAsset._attributes['data-library'] = libraryName;
+
+          // Set up our onload handler.
+          cssAsset._attributes.onload = function() {
+            cssAssetsProcessed++;
+            cssDelta++;
+//            console.log('loaded', cssAsset._attributes.href);
+            if (cssAssetsProcessed < cssAssetCount) {
+//              console.log('loaded a css asset, moving to the next');
+              loadCssAsset();
+            }
+            else {
+//              console.log('all done loading css assets, moving to the next library');
+              librariesProcessed++;
+              libraryDelta++;
+
+              // Mark the library as loaded.
+              var libs = dg.getLibraries();
+              if (!libs[moduleName]) { libs[moduleName] = {}; }
+              if (!libs[moduleName][libraryName]) { libs[moduleName][libraryName] = {}; }
+
+              // Give modules a chance to react to the completion of a loaded library..
+              jDrupal.moduleInvokeAll('library_onload', moduleName, libraryName);
+
+              if (librariesProcessed < libraryCount) {
+                loadLibrary();
+              }
+              else { ok(); }
+            }
+          };
+          dg.addCss(cssAsset);
+
+        };
+
         if (jsAssetsProcessed < jsAssetCount) {
           loadJsAsset();
+        }
+
+        if (cssAssetsProcessed < cssAssetCount) {
+          loadCssAsset();
         }
 
       };
@@ -164,7 +221,7 @@ dg.libraryLoad = function(moduleName, libraryName) {
 
     // Load up the library configuration from the module.
     var library = dg.getModuleLibrary(moduleName, libraryName);
-    
+
     // Specify which asset types we allow and count them.
     var assetTypes = ['js', 'css'];
     var assetTypesCount = assetTypes.length;
@@ -182,7 +239,7 @@ dg.libraryLoad = function(moduleName, libraryName) {
       // The module's library has an asset(s) for this type, count how many.
       assetsLoading += library[assetType].length;
     }
-    
+
     // Iterate over the types of assets we allow...
 
     for (var i = 0; i < assetTypesCount; i++) {
@@ -190,7 +247,7 @@ dg.libraryLoad = function(moduleName, libraryName) {
 
       // If the module's library doesn't provide this asset type, then skip it.
       if (!library[assetType]) { continue; }
-      
+
       // The module's library has an asset(s) for this type, count how many.
       assetCount = library[assetType].length;
 
@@ -223,7 +280,7 @@ dg.libraryLoad = function(moduleName, libraryName) {
         };
 
         console.log('loading asset', asset);
-        
+
         // Add the asset's file to the head, then circle back to the onload handler.
         console.log('LOADING: ' + moduleName + '/' + libraryName);
         assetType == 'js' ? dg.addJs(asset) : dg.addCss(asset);
@@ -256,3 +313,47 @@ dg.getModuleLibrary = function(moduleName, libraryName) {
 dg.getModuleRoutes = function(moduleName) {
   return dg.invoke(moduleName, 'routing');
 };
+
+dg.removeLibraries = function() {
+
+  var libraries = dg.getLibraries();
+//  console.log('libraries', libraries);
+
+  for (var module in libraries) {
+
+    if (!dg.hop(libraries, module)) { continue; }
+
+//    console.log(module, libraries[module]);
+
+    for (var library in libraries[module]) {
+
+      if (!dg.hop(libraries[module], library)) { continue; }
+
+      var options = dg.getModuleLibrary(module, library);
+//      console.log(options);
+
+      var suffix = '[data-module="' + module + '"][data-library="' + library + '"]';
+      var removeElements = function(elements) {
+        for (var i = 0; i < elements.length; i++) {
+          console.log('removing', elements[i]);
+          elements[i].remove();
+        }
+      };
+
+      if (options.js) {
+        var selector = 'head script' + suffix;
+        var scripts = document.querySelectorAll(selector);
+        if (scripts) { removeElements(scripts); }
+      }
+
+      if (options.css) {
+        var selector = 'head link' + suffix;
+        var links = document.querySelectorAll(selector);
+        if (links) { removeElements(links); }
+      }
+
+    }
+
+  }
+
+}
