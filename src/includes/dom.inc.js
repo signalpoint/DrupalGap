14799664124ce34a368c4a6d861f0fc454e706b4
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2025-10-12 12:29:59 +0000

    dom - add isChild(), removeJs(), removeCss(), getJs(), jsIsLoaded(), getCss(), cssIsLoaded() - adjust addJs() and addCss() param name and create resultant element prior to sending along to head - modify addToHead() to take in an element and improve its parsing of incoming attributes

--- src/includes/dom.inc.js
+++ src/includes/dom.inc.js
@@ -54,6 +54,17 @@ dg.el = function(el) {
   return dg.qs(el);
 };
 
+dg.isChild = function(child, parent) {
+  var node = child.parentNode;
+  while (node != null) {
+    if (node === parent) {
+      return true;
+    }
+    node = node.parentNode;
+  }
+  return false;
+};
+
 /**
  * A shortcut function to call document.querySelector().
  * @param el {String|Object} The element identifier or element object (as a pass through).
@@ -73,7 +84,7 @@ dg.qsa = function(selectors) {
   return document.querySelectorAll(selectors);
 };
 
-dg.qsi = function(id) { return document.getElementById(id); }
+dg.qsi = function(id) { return document.getElementById(id); };
 
 /**
  * Shows an element.
@@ -106,59 +117,120 @@ dg.isVisible = function(el) {
 
 /**
  * Adds a .js file to the head of the DOM.
- * @param js {Object}
+ * @param options {Object}
  *  _attributes {Object}
  *    src {String} The url or file path of the .js file.
  *    * {*} Any other attributes you'd like added to the script tag.
  */
-dg.addJs = function(js) {
-  dg.addToHead('js', js);
+dg.addJs = function(options) {
+//  console.log('...js', options._attributes.src);
+  var element = document.createElement('script');
+  element.type = 'text/javascript';
+  dg.addToHead(element, options);
+};
+
+dg.removeJs = function(src) {
+  var jsLinks = document.querySelectorAll('script[src]');
+  for (var i = 0; i < jsLinks.length; i++) {
+    if (jsLinks[i].src.indexOf(src) !== -1) {
+//      console.log('-js', src);
+      jsLinks[i].remove();
+    }
+  }
 };
 
 /**
  * Adds a .css file to the head of the DOM.
- * @param css {Object}
+ * @param options {Object}
  *  _attributes {Object}
  *    href {String} The url or file path of the .css file.
  *    * {*} Any other attributes you'd like added to the link tag.
  */
-dg.addCss = function(css) {
-  dg.addToHead('css', css);
+dg.addCss = function(options) {
+//  console.log('...css', options._attributes.href);
+  var element = document.createElement('link');
+  element.rel = 'stylesheet';
+  dg.addToHead(element, options);
+};
+
+dg.removeCss = function(href) {
+  var cssLinks = document.querySelectorAll('link[rel="stylesheet"]');
+  for (var i = 0; i < cssLinks.length; i++) {
+    if (cssLinks[i].href && cssLinks[i].href.indexOf(href) !== -1) {
+//      console.log('-css', href);
+      cssLinks[i].remove();
+    }
+  }
 };
 
 /**
  * @see credit: https://stackoverflow.com/a/22534608/763010
  *
- * Adds a file to the head of the DOM.
- * @param assetType {String} 'js' or 'css'
- * @param asset {Object}
- *    If assetType is 'js' @see addJsToHead().
- *    If assetType is 'css' @see addCssToHead().
+ * Adds a element to the head of the DOM.
+ * @param element - an element for the DOM, e.g. createElement()
+ * @param options {Object} - a typical dg8 element with _attributes
  */
-dg.addToHead = function(assetType, asset) {
-  var head = document.getElementsByTagName('head')[0];
-  var map = {
-    js: 'script',
-    css: 'link'
-  };
-  var attrMap = {
-    js: 'text/javascript',
-    css: 'stylesheet'
-  };
-  var element = document.createElement(map[assetType]);
-  var attributes = asset._attributes;
+dg.addToHead = function(element, options) {
+
+  // Place the attributes onto the element.
+  // - functions get placed directly on the element (because setElement() crashes when setting a value to a function)
+  // - other attributes get set via setAttribute()
+  var attributes = options._attributes;
   for (var name in attributes) {
-    if (!attributes.hasOwnProperty(name)) { continue; }
-    element[name] = attributes[name];
-  }
-  switch (assetType) {
-    case 'js':
-      if (!element.type) { element.type = attrMap[assetType]; }
-      break;
-    case 'css':
-      if (!element.rel) { element.rel = attrMap[assetType]; }
-      break;
+    if (!dg.hop(attributes, name)) { continue; }
+    if (typeof attributes[name] === 'function') { element[name] = attributes[name]; }
+    else if (attributes[name] === null) { element.setAttribute(name, ''); }
+    else { element.setAttribute(name, attributes[name]); }
   }
 
+  // Append the element to the <head>.
+  var head = document.getElementsByTagName('head')[0];
   head.appendChild(element);
+
+};
+
+dg.getJs = function() {
+  var js = document.getElementsByTagName('script');
+  var paths = [];
+  for (var i = 0; i < js.length; i++) {
+    if (js[i].src) {
+      paths.push(js[i].src);
+    }
+  }
+  return paths;
+};
+
+dg.jsIsLoaded = function(path) {
+  var paths = dg.getJs();
+//  console.log('jsPathIsLoaded', path, paths);
+  for (var i = 0; i < paths.length; i++) {
+    var loadedPath = paths[i].split('?')[0];
+    if (loadedPath.indexOf(path) !== -1) {
+      return true;
+    }
+  }
+  return false;
+};
+
+dg.getCss = function() {
+  var css = document.styleSheets;
+  var paths = [];
+  for (var i = 0; i < css.length; i++) {
+    if (css[i].href) {
+      paths.push(css[i].href);
+    }
+  }
+  return paths;
+};
+
+dg.cssIsLoaded = function(path) {
+  var paths = dg.getCss();
+//  console.log('cssPathIsLoaded', path, paths);
+  for (var i = 0; i < paths.length; i++) {
+    var loadedPath = paths[i].split('?')[0];
+    if (loadedPath.indexOf(path) !== -1) {
+      return true;
+    }
+  }
+  return false;
 };
