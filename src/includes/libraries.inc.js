d91a5591ea42c357dfc6d0e9fd8db652f9933545
Tyler Frankenstein <tfrankenstein@crimewatchus.com>
2025-10-12 12:43:34 +0000

    libraries | addAttachments() - remove a bunch of left over code after a return statement | libraryLoad() - remove a bunch of commented out code

--- src/includes/libraries.inc.js
+++ src/includes/libraries.inc.js
@@ -152,57 +152,6 @@ dg.addAttachments = function(content) {
   }
   return Promise.all(promises);
 
-
-
-
-  return;
-
-  // Grab the libraries attached to the content, if any. Then iterate over each of the modules in the attachment to
-  // process their libraries.
-  var promises = [];
-  var attachments = content._attached;
-  for (var moduleName in attachments) {
-    if (!attachments.hasOwnProperty(moduleName)) { continue; }
-
-    // Figure out what kind of attachment it is, a simple array of library names, or a more complex object with
-    // configuration.
-    var attachment = attachments[moduleName];
-    console.log(attachment);
-    return;
-
-
-    for (var i = 0; i < attachment.length; i++) {
-      var libraryName = attachment[i];
-      dg.libraryLoadSync(moduleName, libraryName);
-    }
-    continue;
-
-
-
-
-    if (dg.isObject(attachment)) {
-      if (attachment.async === false) {
-        for (var i = 0; i < attachment.libraries.length; i++) {
-          var libraryName = attachment.libraries[i];
-          dg.libraryLoad(moduleName, libraryName, {
-            async: false
-          });
-        }
-      }
-    }
-    else {
-      if (dg.isString(attachment)) { attachment = [attachment]; } // Convert single strings into an array.
-
-      // load each library on the attachment, queueing up the promises returned by libraryLoad().
-      for (var i = 0; i < attachment.length; i++) {
-        var libraryName = attachment[i];
-        promises.push(dg.libraryLoad(moduleName, libraryName));
-      }
-    }
-
-  }
-  return Promise.all(promises);
-
 };
 
 dg.libraryLoad = function(moduleName, libraryName) {
@@ -220,77 +169,6 @@ dg.libraryLoad = function(moduleName, libraryName) {
     var library = dg.getModuleLibrary(moduleName, libraryName);
 
     dg.libraryAdd(moduleName, libraryName, library, ok, err);
-
-    // ... ABSTRACT OUT ...
-
-//    // Specify which asset types we allow and count them.
-//    var assetTypes = ['js', 'css'];
-//    var assetTypesCount = assetTypes.length;
-//    var assetType = null;
-//    var assetCount = 0;
-//
-//    // First figure out how many assets we are going to load.
-//    var assetsLoading = 0;
-//    for (var i = 0; i < assetTypesCount; i++) {
-//      assetType = assetTypes[i];
-//
-//      // If the module's library doesn't provide this asset type, then skip it.
-//      if (!library[assetType]) { continue; }
-//
-//      // The module's library has an asset(s) for this type, count how many.
-//      assetsLoading += library[assetType].length;
-//    }
-//
-//    // Iterate over the types of assets we allow...
-//
-//    for (var i = 0; i < assetTypesCount; i++) {
-//      assetType = assetTypes[i];
-//
-//      // If the module's library doesn't provide this asset type, then skip it.
-//      if (!library[assetType]) { continue; }
-//
-//      // The module's library has an asset(s) for this type, count how many.
-//      assetCount = library[assetType].length;
-//
-//      // Iterate over each asset of this type...
-//      for (var j = 0; j < assetCount; j++) {
-//        var asset = library[assetType][j];
-//
-//        // Set up our onload handler.
-//        asset._attributes.onload = function() { // @TODO allow library implementors to have their own onload too.
-//
-//          // Mark an asset as loaded.
-//          assetsLoading--;
-//          console.log('done loading asset, assets left: ' + assetsLoading);
-//
-//          // Once we're done loading all the assets...
-//          if (!assetsLoading) {
-//
-//            // Mark the library as loaded.
-//            var libs = dg.getLibraries();
-//            if (!libs[moduleName]) { libs[moduleName] = {}; }
-//            if (!libs[moduleName][libraryName]) { libs[moduleName][libraryName] = {}; }
-//
-//            // Give modules a chance to react to the completion of a loaded library..
-//            jDrupal.moduleInvokeAll('library_onload', moduleName, libraryName);
-//
-//            // Resolve.
-//            ok();
-//          }
-//
-//        };
-//
-//        console.log('loading asset', asset);
-//
-//        // Add the asset's file to the head, then circle back to the onload handler.
-//        console.log('LOADING: ' + moduleName + '/' + libraryName);
-//        assetType == 'js' ? dg.addJs(asset) : dg.addCss(asset);
-//
-//      }
-//
-//    }
-
-    // ... ABSTRACT OUT ...
 
   });
 };
